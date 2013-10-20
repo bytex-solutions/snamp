9de882b3d4b9147a050264f9d84e22cef34b7f59
temni <temni@ubuntu.ubuntu-domain>
2013-10-20 19:23:19 +1100

    Refactored test infrastucture

---
+++ src/test/java/SimpleAgentConfiguration.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: temni
+ * Date: 20.10.13
+ * Time: 17:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleAgentConfiguration {
+}
