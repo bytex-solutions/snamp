6272db0bd7b1fba9be8e1963324f2b35e1144400
sakno <roman.sakno@gmail.com>
2013-10-31 01:08:14 +1100

    Test running fixed

--- tests/src/test/java/SnampTestCase.java
+++ tests/src/test/java/SnampTestCase.java
@@ -1,11 +1,12 @@
 import com.snamp.licensing.LicenseReader;
 import junit.framework.TestCase;
+import org.junit.Assert;
 
 /**
  * Represents an abstract class for building SNAMP tests.
  * @author roman
  */
-public abstract class SnampTestCase extends TestCase {
+public abstract class SnampTestCase extends Assert {
 
     static {
         System.setProperty(LicenseReader.LICENSE_FILE_PROPERTY, "unlimited.lic");
