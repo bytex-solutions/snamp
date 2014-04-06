079e250f8eca6a117031499a8b1c24ed36007d3f
sakno <roman.sakno@gmail.com>
2014-04-06 17:20:58 +1100

    XML-based SNAMP configuration via JAXB

--- agent/src/main/java/com/itworks/snamp/configuration/AbstractAgentConfiguration.java
+++ agent/src/main/java/com/itworks/snamp/configuration/AbstractAgentConfiguration.java
@@ -23,7 +23,16 @@ public abstract class AbstractAgentConfiguration implements AgentConfiguration {
      * @return A new clone of this configuration.
      */
     @Override
-    public abstract AbstractAgentConfiguration clone();
+    public AbstractAgentConfiguration clone(){
+        try {
+            final AbstractAgentConfiguration newInstance = getClass().newInstance();
+            newInstance.load(this);
+            return newInstance;
+        }
+        catch (final ReflectiveOperationException e){
+            return this;
+        }
+    }
 
     private static final void copy(final HostingConfiguration input, final HostingConfiguration output){
         output.setAdapterName(input.getAdapterName());
@@ -86,7 +95,7 @@ public abstract class AbstractAgentConfiguration implements AgentConfiguration {
      * @param input The configuration import source.
      * @param output The configuration import destination.
      */
-    public static final void copy(final AgentConfiguration input, final AgentConfiguration output){
+    public static void copy(final AgentConfiguration input, final AgentConfiguration output){
         if(input == null || output == null) return;
         //import hosting configuration
         copy(input.getAgentHostingConfig(), output.getAgentHostingConfig());
@@ -97,5 +106,114 @@ public abstract class AbstractAgentConfiguration implements AgentConfiguration {
                 return output.newManagementTargetConfiguration();
             }
         });
+    }
+
+    private static boolean paramsAreEqual(final Map<String, String> params1, final Map<String, String> params2){
+        if(params1 == params2) return true;
+        else if(params1 == null) return params2 == null;
+        else if(params2 == null) return false;
+        else if(params1.size() == params2.size()){
+            for(final String key1: params1.keySet())
+                if(Objects.equals(params1.get(key1), params2.get(key1))) continue;
+                else return false;
+            return true;
+        }
+        else return false;
+    }
+
+    public static boolean equals(final HostingConfiguration obj1, final HostingConfiguration obj2){
+        if(obj1 == obj2) return true;
+        else if(obj1 == null) return obj2 == null;
+        else if(obj2 == null) return false;
+        else return Objects.equals(obj1.getAdapterName(), obj2.getAdapterName()) &&
+                    paramsAreEqual(obj1.getHostingParams(), obj2.getHostingParams());
+    }
+
+    public static boolean equals(final ManagementTargetConfiguration target1, final ManagementTargetConfiguration target2){
+        if(target1 == target2) return true;
+        else if(target1 == null) return target2 == null;
+        else if(target2 == null) return false;
+        else return Objects.equals(target1.getNamespace(), target2.getNamespace()) &&
+                    paramsAreEqual(target1.getAdditionalElements(), target2.getAdditionalElements()) &&
+                    Objects.equals(target1.getConnectionString(), target2.getConnectionString()) &&
+                    Objects.equals(target1.getConnectionType(), target2.getConnectionType()) &&
+                    attributesAreEqual(target1.getAttributes(), target2.getAttributes()) &&
+                    eventsAreEqual(target1.getEvents(), target2.getEvents());
+    }
+
+    public static boolean equals(final ManagementTargetConfiguration.EventConfiguration ev1, final ManagementTargetConfiguration.EventConfiguration ev2){
+        if(ev1 == ev2) return true;
+        else if(ev1 == null) return ev2 == null;
+        else if(ev2 == null) return false;
+        else return Objects.equals(ev1.getCategory(), ev2.getCategory()) &&
+                    paramsAreEqual(ev1.getAdditionalElements(), ev2.getAdditionalElements());
+    }
+
+    private static boolean eventsAreEqual(final Map<String, ManagementTargetConfiguration.EventConfiguration> evs1, final Map<String, ManagementTargetConfiguration.EventConfiguration> evs2) {
+        if(evs1 == evs2) return true;
+        else if(evs1 == null) return evs2 == null;
+        else if(evs2 == null) return false;
+        else if(evs1.size() == evs2.size()){
+            for(final String key1: evs1.keySet())
+                if(equals(evs1.get(key1), evs2.get(key1))) continue;
+                else return false;
+            return true;
+        }
+        else return false;
+    }
+
+    /**
+     * Determines whether the attribute descriptors are structurally equal.
+     * @param attr1 The first attribute descriptor to compare.
+     * @param attr2 The second attribute descriptor to compare.
+     * @return {@literal true}, if both descriptors are structurally equal; otherwise, {@literal false}.
+     */
+    public static boolean equals(final ManagementTargetConfiguration.AttributeConfiguration attr1, final ManagementTargetConfiguration.AttributeConfiguration attr2){
+        if(attr1 == attr2) return true;
+        else if(attr1 == null) return attr2 == null;
+        else if(attr2 == null) return false;
+        else return Objects.equals(attr1.getAttributeName(), attr2.getAttributeName()) &&
+                    Objects.equals(attr1.getReadWriteTimeout(), attr2.getReadWriteTimeout()) &&
+                    paramsAreEqual(attr1.getAdditionalElements(), attr2.getAdditionalElements());
+    }
+
+    private static boolean attributesAreEqual(final Map<String, ManagementTargetConfiguration.AttributeConfiguration> attrs1, final Map<String, ManagementTargetConfiguration.AttributeConfiguration> attrs2) {
+        if(attrs1 == attrs2) return true;
+        else if(attrs1 == null) return attrs2 == null;
+        else if(attrs2 == null) return false;
+        else if(attrs1.size() == attrs2.size()){
+            for(final String key1: attrs1.keySet())
+                if(equals(attrs1.get(key1), attrs2.get(key1))) continue;
+                else return false;
+            return true;
+        }
+        else return false;
+    }
+
+    private static boolean targetsAreEqual(final Map<String, ManagementTargetConfiguration> obj1, final Map<String, ManagementTargetConfiguration> obj2){
+        if(obj1 == obj2) return true;
+        else if(obj1 == null) return obj2 == null;
+        else if(obj2 == null) return false;
+        else if(obj1.size() == obj2.size()){
+            for(final String key1: obj1.keySet())
+                if(equals(obj1.get(key1), obj2.get(key1))) continue;
+                else return false;
+            return true;
+        }
+        else return false;
+    }
+
+    /**
+     * Determines whether the two configurations are structurally equal.
+     * @param obj1 The first configuration to compare.
+     * @param obj2 The second configuration to compare.
+     * @return {@literal true}, if configurations are structurally equal; otherwise, {@literal false}.
+     */
+    public static boolean equals(final AgentConfiguration obj1, final AgentConfiguration obj2){
+        if(obj1 == obj2) return true;
+        else if(obj1 == null) return obj2 == null;
+        else if(obj2 == null) return false;
+        else return equals(obj1.getAgentHostingConfig(), obj2.getAgentHostingConfig()) &&
+                    targetsAreEqual(obj1.getTargets(), obj2.getTargets());
     }
 }
---
+++ tests/src/test/java/com/itworks/snamp/configuration/XmlAgentConfigurationTest.java
@@ -0,0 +1,34 @@
+package com.itworks.snamp.configuration;
+
+import com.itworks.snamp.SnampClassTestSet;
+import com.itworks.snamp.TimeSpan;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author Roman Sakno
+ * @version 1.0
+ * @since 1.0
+ */
+public final class XmlAgentConfigurationTest extends SnampClassTestSet<XmlAgentConfiguration> {
+    @Test
+    public final void serializationTest() throws IOException {
+        final XmlAgentConfiguration config = new XmlAgentConfiguration();
+        config.getAgentHostingConfig().setAdapterName("snmp");
+        config.getAgentHostingConfig().getHostingParams().put("port", "22");
+        config.getAgentHostingConfig().getHostingParams().put("address", "localhost");
+        final AgentConfiguration.ManagementTargetConfiguration target = config.newManagementTargetConfiguration();
+        config.getTargets().put("test-server", target);
+        target.setConnectionString("http://connection-string");
+        target.setConnectionType("jmx");
+        final AgentConfiguration.ManagementTargetConfiguration.AttributeConfiguration attr = target.newAttributeConfiguration();
+        attr.setAttributeName("attribute1");
+        attr.setReadWriteTimeout(new TimeSpan(100));
+        target.getAttributes().put("test-attribute", attr);
+        final String content = config.toXmlString();
+        final XmlAgentConfiguration newConfig = new XmlAgentConfiguration();
+        newConfig.fromXmlString(content);
+        assertTrue(XmlAgentConfiguration.equals(newConfig, config));
+    }
+}
