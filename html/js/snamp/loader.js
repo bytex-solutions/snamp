dbcbc73cec5150bf6a1b6fc2831a04b43d237a22
temni <evgeniy.kirichenko@gmail.com>
2014-04-27 14:32:38 +1100

    Operations button panel has been appended

--- html/js/snamp/loader.js
+++ html/js/snamp/loader.js
@@ -3,7 +3,7 @@
   $.fn.createTable = function( options ) {  
     // Making the header part of table
     var table = $("<table>", {class:"table"});
-    table.append("<thead><tr><th>Status</th><th>Bundle Name</th><th>Description</th><th>Operations</th></tr></thead>");
+    table.append("<thead><tr><th>Status</th><th>Type</th><th>Bundle Name</th><th>Description</th><th width=\"15%\">Operations</th></tr></thead>");
 
     
     // Making data request
@@ -24,11 +24,18 @@
       content = "<img src=\"" + data[i].status + ".png" +"\"/>";
       tr.append("<td>" + content + "</td>");
 
+      content = data[i].type;
+      tr.append("<td>" + content + "</td>");
+
       content = data[i].name;
       tr.append("<td>" + content + "</td>");
 
       content = data[i].description;
       tr.append("<td>" + content + "</td>");
+
+      var td = $("<td></td>");
+      td.appendTo(tr);
+      td.addOperations(data[i].name, data[i].status);
 
       tbody.append(tr);
     };
@@ -38,4 +45,38 @@
     // Appending the table to the element
     table.appendTo(this);
   };
+
+  $.fn.addOperations = function( UUID, status )
+  {
+      var btnGrp = $("<div>", { class: "btn-group" });
+
+      // start button
+      var btnStart = $("<button>", { type: "button", class: "btn btn-default btn-xs"} );
+      if (status == true) btnStart.attr("disabled", "disabled");
+      var span = $("<span>", { class : "glyphicon glyphicon-play"});
+      span.appendTo(btnStart);
+      btnStart.appendTo(btnGrp);
+
+      // stop button
+      var btnStop = $("<button>", { type: "button", class: "btn btn-default btn-xs"} );
+      if (status == false) btnStop.attr("disabled", "disabled");
+      var span = $("<span>", { class : "glyphicon glyphicon-stop"});
+      span.appendTo(btnStop);
+      btnStop.appendTo(btnGrp);
+
+      // refresh button
+      var btnRefresh = $("<button>", { type: "button", class: "btn btn-default btn-xs"} );
+      var span = $("<span>", { class : "glyphicon glyphicon-refresh"});
+      span.appendTo(btnRefresh);
+      btnRefresh.appendTo(btnGrp);
+
+      // remove button
+      var btnRemove = $("<button>", { type: "button", class: "btn btn-default btn-xs"} );
+      var span = $("<span>", { class : "glyphicon glyphicon-remove"});
+      span.appendTo(btnRemove);
+      btnRemove.appendTo(btnGrp);
+
+      this.append(btnGrp);
+  };
+
 })( jQuery );
--- html/js/snamp/stubs.js
+++ html/js/snamp/stubs.js
@@ -3,9 +3,9 @@
   $.fn.getBundleInfo = function() {  
     // activeStatus, bundle name, description
     var bundleInfo = [
-        {status: true, name: "JMX connector", description: "Provides information about remote or local JMX bean according to configuration file propertiess"},
-        {status: true, name: "SNMP adapter", description: "Provides SNMP OIDs from any source"},
-        {status: false, name: "WMB connector", description: "Links to Websphere Message Broker and returns some information from there"}
+        {status: true, type: "connector", name: "JMX connector", description: "Provides information about remote or local JMX bean according to configuration file propertiess"},
+        {status: true, type: "adapter",  name: "SNMP adapter", description: "Provides SNMP OIDs from any source"},
+        {status: false, type: "connector", name: "WMB connector", description: "Links to Websphere Message Broker and returns some information from there"}
     ];
     return bundleInfo;
   };
