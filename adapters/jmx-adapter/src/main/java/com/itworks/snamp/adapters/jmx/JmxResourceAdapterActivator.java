4a6175135b08c966306ce7c4e27c747aa7f9fc1a
sakno <roman.sakno@gmail.com>
2014-06-28 13:04:54 +1100

    Configuration and licensing support for JMX adapter

--- adapters/jmx-adapter/src/main/java/com/itworks/snamp/adapters/jmx/JmxResourceAdapterActivator.java
+++ adapters/jmx-adapter/src/main/java/com/itworks/snamp/adapters/jmx/JmxResourceAdapterActivator.java
@@ -7,9 +7,7 @@ import javax.management.ObjectName;
 import java.net.MalformedURLException;
 import java.util.Map;
 
-import static com.itworks.snamp.adapters.jmx.JmxAdapterConfigurationProvider.DEBUG_USE_PURE_SERIALIZATION_PARAM;
-import static com.itworks.snamp.adapters.jmx.JmxAdapterConfigurationProvider.OBJECT_NAME_PARAM;
-import static com.itworks.snamp.adapters.jmx.JmxAdapterConfigurationProvider.USE_PLATFORM_MBEAN_PARAM;
+import static com.itworks.snamp.adapters.jmx.JmxAdapterConfigurationProvider.*;
 
 /**
  * Represents JMX resource adapter activator.
@@ -18,12 +16,21 @@ import static com.itworks.snamp.adapters.jmx.JmxAdapterConfigurationProvider.USE
  * @since 1.0
  */
 public final class JmxResourceAdapterActivator extends AbstractResourceAdapterActivator<JmxResourceAdapter> {
+    private static final class JmxConfigurationDescriptor extends ConfigurationEntityDescriptionProviderHolder<JmxAdapterConfigurationProvider>{
+
+        @Override
+        protected JmxAdapterConfigurationProvider createConfigurationDescriptionProvider(final RequiredService<?>... dependencies) throws Exception {
+            return new JmxAdapterConfigurationProvider();
+        }
+    }
+
     /**
      * Initializes a new instance of the resource adapter lifetime manager.
      */
     public JmxResourceAdapterActivator() {
         super(JmxAdapterHelpers.ADAPTER_NAME,
-                JmxAdapterHelpers.getLogger());
+                JmxAdapterHelpers.getLogger(),
+                new JmxConfigurationDescriptor());
     }
 
     /**
@@ -37,11 +44,12 @@ public final class JmxResourceAdapterActivator extends AbstractResourceAdapterAc
      */
     @Override
     protected JmxResourceAdapter createAdapter(final Map<String, String> parameters, final Map<String, ManagedResourceConfiguration> resources, final RequiredService<?>... dependencies) throws Exception {
-        if(parameters.containsKey(OBJECT_NAME_PARAM)){
+        if (parameters.containsKey(OBJECT_NAME_PARAM)) {
+            JmxAdapterLicenseLimitations.current().verifyServiceVersion(JmxResourceAdapter.class);
             final JmxResourceAdapter adapter = new JmxResourceAdapter(new ObjectName(parameters.get(OBJECT_NAME_PARAM)),
-                parameters.containsKey(USE_PLATFORM_MBEAN_PARAM) && Boolean.valueOf(parameters.get(USE_PLATFORM_MBEAN_PARAM)),
+                    parameters.containsKey(USE_PLATFORM_MBEAN_PARAM) && Boolean.valueOf(parameters.get(USE_PLATFORM_MBEAN_PARAM)),
                     resources);
-            if(parameters.containsKey(DEBUG_USE_PURE_SERIALIZATION_PARAM) && Boolean.valueOf(parameters.get(DEBUG_USE_PURE_SERIALIZATION_PARAM)))
+            if (parameters.containsKey(DEBUG_USE_PURE_SERIALIZATION_PARAM) && Boolean.valueOf(parameters.get(DEBUG_USE_PURE_SERIALIZATION_PARAM)))
                 adapter.usePureSerialization();
             return adapter;
         }
