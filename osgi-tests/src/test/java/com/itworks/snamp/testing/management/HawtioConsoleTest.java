a44da77f0b5860af4e3a9e78a705a03e0cb633bc
ekiriche <evgeny.kirichenko@t-systems.ru>
2015-02-26 16:22:21 +0300

    Insufficient roles/credentials for operation exception still occurs.

---
+++ osgi-tests/src/test/java/com/itworks/snamp/testing/management/HawtioConsoleTest.java
@@ -0,0 +1,38 @@
+package com.itworks.snamp.testing.management;
+
+import com.itworks.snamp.configuration.AgentConfiguration;
+import com.itworks.snamp.testing.AbstractSnampIntegrationTest;
+import com.itworks.snamp.testing.SnampDependencies;
+import com.itworks.snamp.testing.SnampFeature;
+import org.junit.Test;
+
+/**
+ * Description here
+ *
+ * @author Evgeniy Kirichenko
+ * @date 26.02.2015
+ */
+@SnampDependencies(SnampFeature.SNMP_CONNECTOR)
+public class HawtioConsoleTest  extends AbstractSnampIntegrationTest {
+
+    /**
+     * Simple test.
+     *
+     * @throws InterruptedException the interrupted exception
+     */
+    @Test
+    public void simpleTest() throws InterruptedException {
+        while(true) {
+            Thread.sleep(1000);
+        }
+    }
+
+    /**
+     * Leave it empty.
+     * @param config The configuration to set.
+     */
+    @Override
+    protected void setupTestConfiguration(AgentConfiguration config) {
+
+    }
+}
