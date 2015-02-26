8cc06a32ba290a6950571c35778e5938f0201927
ekiriche <evgeny.kirichenko@t-systems.ru>
2015-02-26 15:17:35 +0300

    Appended all resources to the skeleton of the snamp management console plugin

---
+++ infrastructure/hawtio-console/src/main/java/com/itworks/snamp/hawtio/PluginContextListener.java
@@ -0,0 +1,37 @@
+package com.itworks.snamp.hawtio;
+
+import io.hawt.web.plugin.HawtioPlugin;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Plugin context listener.
+ *
+ * @author Evgeniy Kirichenko
+ */
+public class PluginContextListener implements ServletContextListener {
+
+
+    HawtioPlugin plugin = null;
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        final ServletContext context = servletContextEvent.getServletContext();
+
+        plugin = new HawtioPlugin();
+        plugin.setContext(context.getInitParameter("plugin-context"));
+        plugin.setName(context.getInitParameter("plugin-name"));
+        plugin.setScripts(context.getInitParameter("plugin-scripts"));
+        plugin.setDomain(null);
+        plugin.init();
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        plugin.destroy();
+    }
+}
