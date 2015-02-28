ce1ecb1760b4082d3c2790b2ed406ccbbbadda80
Evgeniy Kirichenko <evgeniy.kirichenko@gmail.com>
2015-02-28 12:40:16 +0300

    Now it works fine, the mistake was in the configuration - with a blank configuration we always get NULL for JSON value due to composite data is empty (except two keys in the main tree object)

--- osgi-tests/src/test/java/com/itworks/snamp/testing/management/HawtioConsoleTest.java
+++ osgi-tests/src/test/java/com/itworks/snamp/testing/management/HawtioConsoleTest.java
@@ -1,10 +1,23 @@
 package com.itworks.snamp.testing.management;
 
+import com.google.common.base.Supplier;
+import com.itworks.snamp.ExceptionalCallable;
+import com.itworks.snamp.TimeSpan;
+import com.itworks.snamp.adapters.ResourceAdapterActivator;
 import com.itworks.snamp.configuration.AgentConfiguration;
-import com.itworks.snamp.testing.AbstractSnampIntegrationTest;
 import com.itworks.snamp.testing.SnampDependencies;
 import com.itworks.snamp.testing.SnampFeature;
+import com.itworks.snamp.testing.connectors.jmx.AbstractJmxConnectorTest;
+import com.itworks.snamp.testing.connectors.jmx.TestOpenMBean;
 import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import java.util.Map;
+
+import static com.itworks.snamp.testing.connectors.jmx.TestOpenMBean.BEAN_NAME;
 
 /**
  * Description here
@@ -12,8 +25,21 @@ import org.junit.Test;
  * @author Evgeniy Kirichenko
  * @date 26.02.2015
  */
-@SnampDependencies({SnampFeature.SNMP_CONNECTOR, SnampFeature.JMX_ADAPTER})
-public class HawtioConsoleTest  extends AbstractSnampIntegrationTest {
+@SnampDependencies({SnampFeature.SNMP_ADAPTER})
+public class HawtioConsoleTest extends AbstractJmxConnectorTest<TestOpenMBean> {
+
+    private static final String ADAPTER_NAME = "snmp";
+    private static final String SNMP_PORT = "3222";
+    private static final String SNMP_HOST = "127.0.0.1";
+
+    /**
+     * Instantiates a new Snamp manager test.
+     *
+     * @throws javax.management.MalformedObjectNameException the malformed object name exception
+     */
+    public HawtioConsoleTest() throws MalformedObjectNameException {
+        super(new TestOpenMBean(), new ObjectName(TestOpenMBean.BEAN_NAME));
+    }
 
     @Override
     protected boolean enableRemoteDebugging() {
@@ -32,12 +58,109 @@ public class HawtioConsoleTest  extends AbstractSnampIntegrationTest {
         }
     }
 
-    /**
-     * Leave it empty.
-     * @param config The configuration to set.
-     */
     @Override
-    protected void setupTestConfiguration(AgentConfiguration config) {
+    protected void beforeStartTest(final BundleContext context) throws Exception {
+        super.beforeStartTest(context);
+        beforeCleanupTest(context);
+    }
 
+    @Override
+    protected void afterStartTest(final BundleContext context) throws Exception {
+        startResourceConnector(context);
+        syncWithAdapterStartedEvent(ADAPTER_NAME, new ExceptionalCallable<Void, BundleException>() {
+            @Override
+            public Void call() throws BundleException {
+                ResourceAdapterActivator.startResourceAdapter(context, ADAPTER_NAME);
+                return null;
+            }
+        }, TimeSpan.fromSeconds(4));
+    }
+
+    @Override
+    protected void beforeCleanupTest(final BundleContext context) throws Exception {
+        ResourceAdapterActivator.stopResourceAdapter(context, ADAPTER_NAME);
+        stopResourceConnector(context);
+    }
+
+    @Override
+    protected void fillAdapters(final Map<String, AgentConfiguration.ResourceAdapterConfiguration> adapters, final Supplier<AgentConfiguration.ResourceAdapterConfiguration> adapterFactory) {
+        final AgentConfiguration.ResourceAdapterConfiguration snmpAdapter = adapterFactory.get();
+        snmpAdapter.setAdapterName(ADAPTER_NAME);
+        snmpAdapter.getParameters().put("port", SNMP_PORT);
+        snmpAdapter.getParameters().put("host", SNMP_HOST);
+        snmpAdapter.getParameters().put("socketTimeout", "5000");
+        adapters.put("test-snmp", snmpAdapter);
+    }
+
+    @Override
+    protected void fillAttributes(final Map<String, AgentConfiguration.ManagedResourceConfiguration.AttributeConfiguration> attributes, final Supplier<AgentConfiguration.ManagedResourceConfiguration.AttributeConfiguration> attributeFactory) {
+        AgentConfiguration.ManagedResourceConfiguration.AttributeConfiguration attribute = attributeFactory.get();
+        attribute.setAttributeName("string");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.1.0");
+        attributes.put("1.0", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("boolean");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.2.0");
+        attributes.put("2.0", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("int32");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.3.0");
+        attributes.put("3.0", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("bigint");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.4.0");
+        attributes.put("4.0", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("array");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.5.1");
+        attributes.put("5.1", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("dictionary");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.6.1");
+        attributes.put("6.1", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("table");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.7.1");
+        attributes.put("7.1", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("float");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.8.0");
+        attributes.put("8.0", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("date");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("displayFormat", "yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        attribute.getParameters().put("oid", "1.1.9.0");
+        attributes.put("9.0", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("date");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("displayFormat", "rfc1903-human-readable");
+        attribute.getParameters().put("oid", "1.1.10.0");
+        attributes.put("10.0", attribute);
+
+        attribute = attributeFactory.get();
+        attribute.setAttributeName("date");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("displayFormat", "rfc1903");
+        attribute.getParameters().put("oid", "1.1.11.0");
+        attributes.put("11.0", attribute);
     }
 }
