bd6139feb5f133607d5d0c26b7d17274607ca6f0
Roman Sakno <rsakno@yotateam.com>
2015-05-20 17:28:48 +0300

    Notification support and Composite/Tabular data types for Groovy connector

--- connectors/groovy-connector/src/main/java/com/itworks/snamp/connectors/groovy/AttributeScript.java
+++ connectors/groovy-connector/src/main/java/com/itworks/snamp/connectors/groovy/AttributeScript.java
@@ -2,13 +2,17 @@ package com.itworks.snamp.connectors.groovy;
 
 import com.itworks.snamp.connectors.attributes.AttributeSpecifier;
 import com.itworks.snamp.internal.annotations.SpecialUse;
+import com.itworks.snamp.jmx.CompositeDataUtils;
+import com.itworks.snamp.jmx.CompositeTypeBuilder;
+import com.itworks.snamp.jmx.TabularTypeBuilder;
 
 import javax.management.ObjectName;
-import javax.management.openmbean.OpenType;
-import javax.management.openmbean.SimpleType;
+import javax.management.openmbean.*;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -20,6 +24,9 @@ import java.util.Objects;
 public abstract class AttributeScript extends ResourceFeatureScript implements AttributeAccessor {
     private static final String GET_VALUE_METHOD = "getValue";
     private static final String SET_VALUE_METHOD = "setValue";
+    private static final String ITEM_DESCR = "description";
+    private static final String ITEM_TYPE = "type";
+    private static final String ITEM_INDEXED = "indexed";
     @SpecialUse
     protected static final SimpleType<Byte> INT8 = SimpleType.BYTE;
     @SpecialUse
@@ -47,7 +54,109 @@ public abstract class AttributeScript extends ResourceFeatureScript implements A
 
     private OpenType<?> openType = STRING;
 
+    private static String getDescription(final Map item,
+                                         final String fallback) {
+        return item.containsKey(ITEM_DESCR) ?
+                Objects.toString(item.get(ITEM_DESCR)) : fallback;
+    }
+
+    private static OpenType<?> getType(final Map item) throws OpenDataException{
+        final Object result = item.get(ITEM_TYPE);
+        if(result instanceof OpenType<?>)
+            return (OpenType<?>)result;
+        else throw new OpenDataException("Item type is not declared");
+    }
+
+    private static boolean isIndexed(final Map column){
+        if(column.containsKey(ITEM_INDEXED)) {
+            final Object indexed = column.get(ITEM_INDEXED);
+            if (indexed instanceof Boolean)
+                return (Boolean) indexed;
+            else if (indexed instanceof String)
+                return ((String) indexed).length() > 0;
+            else return indexed != null;
+        }
+        else return false;
+    }
+
     //<editor-fold desc="Script helpers">
+
+    /**
+     * Declares dictionary type.
+     * @param typeName Dictionary type name.
+     * @param typeDescription Type description.
+     * @param items Definition of dictionary items.
+     * @return Dictionary type definition.
+     * @throws OpenDataException Invalid type definition.
+     */
+    @SpecialUse
+    protected static CompositeType DICTIONARY(final String typeName,
+                                                final String typeDescription,
+                                                final Map<String, ?> items) throws OpenDataException {
+
+        final CompositeTypeBuilder builder = new CompositeTypeBuilder(typeName, typeDescription);
+        for (final Map.Entry<String, ?> item : items.entrySet())
+            if (item.getValue() instanceof Map) {
+                final String itemName = item.getKey();
+                final String itemDescription = getDescription((Map) item.getValue(), itemName);
+                final OpenType<?> itemType = getType((Map) item.getValue());
+                builder.addItem(itemName, itemDescription, itemType);
+            }
+        return builder.build();
+    }
+
+    private static CompositeData asDictionary(final CompositeType type,
+                                              final Map<String, ?> items) throws OpenDataException{
+        return new CompositeDataSupport(type, items);
+    }
+
+    @SpecialUse
+    protected final CompositeData asDictionary(final Map<String, ?> items) throws OpenDataException{
+        final OpenType<?> t = type();
+        if(t instanceof CompositeType)
+            return asDictionary((CompositeType)t, items);
+        else throw new OpenDataException(String.format("Expected dictionary type but '%s' found", t));
+    }
+
+    private static TabularData asTable(final TabularType type,
+                                       final Collection<Map<String, ?>> rows) throws OpenDataException{
+        final TabularDataSupport result = new TabularDataSupport(type, rows.size() + 5, 0.75f);
+        for(final Map<String, ?> row: rows)
+            result.put(asDictionary(type.getRowType(), row));
+        return result;
+    }
+
+    @SpecialUse
+    protected final TabularData asTable(final Collection<Map<String, ?>> rows) throws OpenDataException{
+        final OpenType<?> t = type();
+        if(t instanceof TabularType)
+            return asTable((TabularType) t, rows);
+        else throw new OpenDataException(String.format("Expected dictionary type but '%s' found", t));
+    }
+
+    /**
+     * Declares table type.
+     * @param typeName Table type name.
+     * @param typeDescription Type description.
+     * @param columns Columns definition.
+     * @return Table type definition.
+     * @throws OpenDataException Invalid type definition.
+     */
+    @SpecialUse
+    protected static TabularType TABLE(final String typeName,
+                                      final String typeDescription,
+                                      final Map<String, ?> columns) throws OpenDataException{
+        final TabularTypeBuilder builder = new TabularTypeBuilder(typeName, typeDescription);
+        for (final Map.Entry<String, ?> column : columns.entrySet())
+            if (column.getValue() instanceof Map) {
+                final String columnName = column.getKey();
+                final String columnDescr = getDescription((Map) column.getValue(), columnName);
+                final OpenType<?> columnType = getType((Map) column.getValue());
+                final boolean indexed = isIndexed((Map)column.getValue());
+                builder.addColumn(columnName, columnDescr, columnType, indexed);
+            }
+        return builder.build();
+    }
 
     /**
      * Sets type of this attribute.
--- connectors/groovy-connector/src/main/java/com/itworks/snamp/connectors/groovy/ManagementScript.java
+++ connectors/groovy-connector/src/main/java/com/itworks/snamp/connectors/groovy/ManagementScript.java
@@ -1,23 +1,24 @@
 package com.itworks.snamp.connectors.groovy;
 
 import com.google.common.base.Supplier;
+import com.itworks.snamp.ArrayUtils;
 import com.itworks.snamp.Consumer;
 import com.itworks.snamp.SafeConsumer;
 import com.itworks.snamp.connectors.ManagedResourceConnector;
 import com.itworks.snamp.connectors.ManagedResourceConnectorClient;
+import com.itworks.snamp.connectors.notifications.NotificationSupport;
+import com.itworks.snamp.connectors.notifications.TypeBasedNotificationFilter;
 import com.itworks.snamp.core.OSGiLoggingContext;
 import com.itworks.snamp.internal.Utils;
 import com.itworks.snamp.internal.annotations.SpecialUse;
 import com.itworks.snamp.jmx.DescriptorUtils;
 import com.itworks.snamp.jmx.JMExceptionUtils;
+import groovy.lang.Closure;
 import groovy.lang.Script;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
-import javax.management.Attribute;
-import javax.management.AttributeNotFoundException;
-import javax.management.JMException;
-import javax.management.MBeanAttributeInfo;
+import javax.management.*;
 import java.util.Dictionary;
 import java.util.Objects;
 import java.util.logging.Logger;
@@ -31,10 +32,82 @@ import java.util.logging.Logger;
 abstract class ManagementScript extends Script {
     private static final String LOGGER_NAME = ResourceConnectorInfo.getLoggerName();
 
-    private static abstract class AttributeOperation<E extends JMException> implements Consumer<ManagedResourceConnector, E>{
+    private static abstract class NotificationOperation<E extends JMException> implements Consumer<ManagedResourceConnector, E> {
+        protected abstract void accept(final NotificationSupport connector) throws E;
+
+        @Override
+        public final void accept(final ManagedResourceConnector connector) throws E {
+            accept(connector.queryObject(NotificationSupport.class));
+        }
+    }
+
+    private static final class NotificationAddListener extends NotificationOperation<MBeanException> {
+        private final NotificationListener listener;
+        private final NotificationFilter filter;
+        private final Object handback;
+
+        private NotificationAddListener(final NotificationListener listener,
+                                        final NotificationFilter filter,
+                                        final Object handback) {
+            this.listener = Objects.requireNonNull(listener);
+            this.filter = filter;
+            this.handback = handback;
+        }
+
+        @Override
+        protected void accept(final NotificationSupport connector) throws MBeanException {
+            if (connector == null)
+                throw new MBeanException(new NullPointerException("Managed resource doesn't support notifications"));
+            else
+                connector.addNotificationListener(listener, filter, handback);
+        }
+    }
+
+    private static final class NotificationRemoveListener extends NotificationOperation<ListenerNotFoundException> {
+        private final NotificationListener listener;
+
+        private NotificationRemoveListener(final NotificationListener listener) {
+            this.listener = Objects.requireNonNull(listener);
+        }
+
+        @Override
+        protected void accept(final NotificationSupport connector) throws ListenerNotFoundException {
+            if (connector == null)
+                throw new ListenerNotFoundException("Managed resource doesn't support notifications");
+            else
+                connector.removeNotificationListener(listener);
+        }
+    }
+
+    private static final class NotificationMetaReader extends NotificationOperation<MBeanException> implements Supplier<Dictionary<String, ?>> {
+        private Dictionary<String, ?> metadata;
+        private final String notificationType;
+
+        private NotificationMetaReader(final String notificationType) {
+            this.notificationType = notificationType;
+        }
+
+        @Override
+        protected void accept(final NotificationSupport connector) throws MBeanException {
+            if (connector == null)
+                throw new MBeanException(new NullPointerException("Managed resource doesn't support notifications"));
+            for (final MBeanNotificationInfo notification : connector.getNotificationInfo())
+                if (ArrayUtils.contains(notification.getNotifTypes(), notificationType)) {
+                    metadata = DescriptorUtils.asDictionary(notification.getDescriptor());
+                    return;
+                }
+        }
+
+        @Override
+        public Dictionary<String, ?> get() {
+            return metadata;
+        }
+    }
+
+    private static abstract class AttributeOperation<E extends JMException> implements Consumer<ManagedResourceConnector, E> {
         private final String attributeName;
 
-        private AttributeOperation(final String name){
+        private AttributeOperation(final String name) {
             this.attributeName = name;
         }
 
@@ -149,7 +222,7 @@ abstract class ManagementScript extends Script {
         return Utils.getBundleContextByObject(this);
     }
 
-    final <E extends Throwable> void processResourceConnector(final String resourceName,
+    private <E extends Throwable> void processResourceConnector(final String resourceName,
                                                               final Consumer<ManagedResourceConnector, E> consumer) throws E {
         final BundleContext context = getBundleContext();
         final ServiceReference<ManagedResourceConnector> connectorRef =
@@ -184,14 +257,15 @@ abstract class ManagementScript extends Script {
 
     /**
      * Reads attribute metadata.
-     * @param resourceName The name of the managed resource.
+     *
+     * @param resourceName  The name of the managed resource.
      * @param attributeName The name of the attribute.
      * @return A dictionary of attribute parameters.
      * @throws AttributeNotFoundException The attribute doesn't exist in the specified managed resource.
      */
     @SpecialUse
     protected final Dictionary<String, ?> getResourceAttributeInfo(final String resourceName,
-                                                                   final String attributeName) throws AttributeNotFoundException{
+                                                                   final String attributeName) throws AttributeNotFoundException {
         final AttributeMetaReader reader = new AttributeMetaReader(attributeName);
         processResourceConnector(resourceName, reader);
         return reader.get();
@@ -199,9 +273,10 @@ abstract class ManagementScript extends Script {
 
     /**
      * Sets value of the managed resource attribute.
-     * @param resourceName The name of the managed resource.
+     *
+     * @param resourceName  The name of the managed resource.
      * @param attributeName The name of the attribute.
-     * @param value The value of the attribute.
+     * @param value         The value of the attribute.
      * @throws JMException Unable to set attribute value.
      */
     @SpecialUse
@@ -209,5 +284,41 @@ abstract class ManagementScript extends Script {
                                               final String attributeName,
                                               final Object value) throws JMException {
         processResourceConnector(resourceName, new AttributeValueWriter(attributeName, value));
+    }
+
+    /**
+     * Reads notification metadata.
+     *
+     * @param resourceName The name of the managed resource.
+     * @param notifType    The notification type as it configured in the managed resource.
+     * @return A dictionary of attribute parameters.
+     * @throws MBeanException Notification is not declared by managed resource.
+     */
+    @SpecialUse
+    protected final Dictionary<String, ?> getResourceNotificationInfo(final String resourceName,
+                                                                      final String notifType) throws MBeanException {
+        final NotificationMetaReader reader = new NotificationMetaReader(notifType);
+        processResourceConnector(resourceName, reader);
+        return reader.get();
+    }
+
+    @SpecialUse
+    protected final void addNotificationListener(final String resourceName,
+                                                 final NotificationListener listener) throws MBeanException {
+        addNotificationListener(resourceName, listener, null, null);
+    }
+
+    @SpecialUse
+    protected final void addNotificationListener(final String resourceName,
+                                                 final NotificationListener listener,
+                                                 final NotificationFilter filter,
+                                                 final Objects handback) throws MBeanException {
+        processResourceConnector(resourceName, new NotificationAddListener(listener, filter, handback));
+    }
+
+    @SpecialUse
+    protected final void removeNotificationListener(final String resourceName,
+                                                    final NotificationListener listener) throws ListenerNotFoundException {
+        processResourceConnector(resourceName, new NotificationRemoveListener(listener));
     }
 }
