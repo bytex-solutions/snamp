a1554a5c577034e6a26ff12dcdf5ffc4f745a336
Evgeniy Kirichenko <evgeniy.kirichenko@gmail.com>
2015-10-05 21:17:28 +0300

    Css styles have been appended

--- docs/md2html.sh
+++ docs/md2html.sh
@@ -6,20 +6,22 @@ mkdir html/adapters
 mkdir html/images
 mkdir html/connectors
 mkdir html/webconsole
+mkdir html/css
 
 cp -avr ./images/*.png ./html/images
 cp -avr ./adapters/*.png ./html/adapters
 cp -avr ./connectors/*.png ./html/connectors
 cp -avr ./webconsole/*.png ./html/webconsole
-for file in $(ls *.md); do pandoc -f markdown_github -t html --html-q-tags "${file}" > "./html/${file}.html"; done;
+cp -avr ./*.css ./html/css
+for file in $(ls *.md); do pandoc -f markdown_github -t html -c css/pandoc.css --html-q-tags "${file}" > "./html/${file}.html"; done;
 cd adapters
-for file in $(ls *.md); do pandoc -f markdown_github -t html --html-q-tags "${file}" > "../html/adapters/${file}.html"; done;
+for file in $(ls *.md); do pandoc -f markdown_github -t html -c ../css/pandoc.css --html-q-tags "${file}" > "../html/adapters/${file}.html"; done;
 cd ..
 cd connectors
-for file in $(ls *.md); do pandoc -f markdown_github -t html --html-q-tags "${file}" > "../html/connectors/${file}.html"; done;
+for file in $(ls *.md); do pandoc -f markdown_github -t html -c ../css/pandoc.css --html-q-tags "${file}" > "../html/connectors/${file}.html"; done;
 cd ..
 cd webconsole
-for file in $(ls *.md); do pandoc -f markdown_github -t html --html-q-tags "${file}" > "../html/webconsole/${file}.html"; done;
+for file in $(ls *.md); do pandoc -f markdown_github -t html -c ../css/pandoc.css --html-q-tags "${file}" > "../html/webconsole/${file}.html"; done;
 cd ..
 cd html
 find . -type f  -name '*.html' -print0 | xargs -0 sed -i 's/md/md.html/g'
