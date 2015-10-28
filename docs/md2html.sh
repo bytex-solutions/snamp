310b02a814a66816a37b17d7113c2da7c8378a4a
Evgeniy Kirichenko <evgeniy.kirichenko@gmail.com>
2015-10-29 00:12:26 +0300

    Fixed CSS and appended examples folder into final html generated folder

--- docs/md2html.sh
+++ docs/md2html.sh
@@ -7,11 +7,13 @@ mkdir html/images
 mkdir html/connectors
 mkdir html/webconsole
 mkdir html/css
+mkdir html/examples
 
 cp -avr ./images/*.png ./html/images
 cp -avr ./adapters/*.png ./html/adapters
 cp -avr ./connectors/*.png ./html/connectors
 cp -avr ./webconsole/*.png ./html/webconsole
+cp -avr ./examples/*.png ./html/examples
 cp -avr ./*.css ./html/css
 for file in $(ls *.md); do pandoc -f markdown_github -t html -c css/pandoc.css --html-q-tags "${file}" > "./html/${file}.html"; done;
 cd adapters
@@ -22,6 +24,9 @@ for file in $(ls *.md); do pandoc -f markdown_github -t html -c ../css/pandoc.cs
 cd ..
 cd webconsole
 for file in $(ls *.md); do pandoc -f markdown_github -t html -c ../css/pandoc.css --html-q-tags "${file}" > "../html/webconsole/${file}.html"; done;
+cd ..
+cd examples
+for file in $(ls *.md); do pandoc -f markdown_github -t html -c ../css/pandoc.css --html-q-tags "${file}" > "../html/examples/${file}.html"; done;
 cd ..
 cd html
 find . -type f  -name '*.html' -print0 | xargs -0 sed -i 's/md/md.html/g'
