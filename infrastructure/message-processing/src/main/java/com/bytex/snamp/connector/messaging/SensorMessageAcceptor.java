352213c6ba6a8adc9883706289e6aa00bfa939a6
Roman Sakno <roman.sakno@swisstok.ru>
2016-08-29 12:25:07 +0300

    Messaging infrastructure

---
+++ infrastructure/message-processing/src/main/java/com/bytex/snamp/connector/messaging/SensorMessageAcceptor.java
@@ -0,0 +1,7 @@
+package com.bytex.snamp.connector.messaging;
+
+/**
+ * Accepts the raw message from sensor and transform it into well known monitoring event.
+ */
+public interface SensorMessageAcceptor {
+}
