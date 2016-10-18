27c0229ff1c027be169ef9779d0a0a7ce9f6d099
Roman Sakno <roman.sakno@swisstok.ru>
2016-10-18 18:48:30 +0300

    Another version of distributed attributes

--- framework/src/main/java/com/bytex/snamp/connector/attributes/DistributedAttributeRepository.java
+++ framework/src/main/java/com/bytex/snamp/connector/attributes/DistributedAttributeRepository.java
@@ -10,6 +10,7 @@ import java.time.Duration;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentMap;
 import static com.bytex.snamp.internal.Utils.getBundleContextOfObject;
+import static com.bytex.snamp.core.DistributedServices.*;
 
 /**
  * Represents repository for attributes which state should be synchronized across cluster nodes.
@@ -19,7 +20,12 @@ import static com.bytex.snamp.internal.Utils.getBundleContextOfObject;
  * @see AttributesDistributionJob
  */
 public abstract class DistributedAttributeRepository<M extends MBeanAttributeInfo> extends AbstractAttributeRepository<M> {
-    private static final String STORAGE_NAME_POSTIFX = "-attributes";
+    private static final String STORAGE_NAME_POSTFIX = "-attributes";
+    protected enum AttributeType{
+        NORMAL,
+        DISTRIBUTED_EXPIRED,
+        DISTRIBUTED_ACTUAL
+    }
 
     private final ConcurrentMap<String, Object> storage;
     private Duration syncPeriod;
@@ -29,12 +35,38 @@ public abstract class DistributedAttributeRepository<M extends MBeanAttributeInf
                                              final boolean expandable) {
         super(resourceName, attributeMetadataType, expandable);
         final BundleContext context = getBundleContextOfObject(this);
-        storage = DistributedServices.getDistributedStorage(context, getResourceName().concat(STORAGE_NAME_POSTIFX));
+        storage = getDistributedStorage(context, getResourceName().concat(STORAGE_NAME_POSTFIX));
         syncPeriod = Duration.ZERO;
+    }
+
+    private BundleContext getBundleContext(){
+        return getBundleContextOfObject(this);
     }
 
     public final void setSyncPeriod(final Duration value){
         syncPeriod = Objects.requireNonNull(value);
+    }
+
+    protected String getStorageKey(final M metadata){
+        return AttributeDescriptor.getName(metadata);
+    }
+
+    protected Object convertFromStoredValue(final M metadata, final Object storedValue){
+        return storedValue;
+    }
+
+    @Override
+    protected final Object getAttribute(final M metadata) throws Exception {
+        final Timeout timeout = new Timeout(Duration.ofSeconds(1L));
+        final String storageKey = getStorageKey(metadata);
+        if (timeout == null)
+            return convertFromStoredValue(metadata, storage.get(storageKey));
+        else
+            timeout.acceptIfExpired(storage, AttributeDescriptor.getName(metadata), (storage, name) -> {
+                if(isActiveNode(getBundleContext())){
+                    //active node must save its state into
+                }
+            });
     }
 
     /**
