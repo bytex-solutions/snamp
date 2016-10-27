a43e13e43c60c62c76bd3f3cd22507c036d877b1
Roman Sakno <roman.sakno@swisstok.ru>
2016-10-27 14:49:39 +0300

    Remove unsupported connectors

--- osgi-tests/src/test/java/com/bytex/snamp/testing/SnampFeature.java
+++ osgi-tests/src/test/java/com/bytex/snamp/testing/SnampFeature.java
@@ -24,9 +24,7 @@ public enum SnampFeature {
     XMPP_GATEWAY("xmpp-gateway-feature", "2.0.0"),
     GROOVY_CONNECTOR("groovy-connector-feature", "2.0.0"),
     GROOVY_GATEWAY("groovy-gateway-feature", "2.0.0"),
-    MODBUS_CONNECTOR("modbus-connector-feature", "2.0.0"),
-    MDA_CONNECTOR("mda-connector-feature", "2.0.0"),
-    MQ_CONNECTOR("mq-connector-feature", "2.0.0");
+    MODBUS_CONNECTOR("modbus-connector-feature", "2.0.0");
 
     final String featureName;
     final String version;
