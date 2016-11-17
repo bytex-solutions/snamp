95ded084b8194ed0857a54438926ac72b64720a4
sakno <roman.sakno@bytex.solutions>
2016-11-17 23:23:32 +0300

    Gateway stub for InfluxDB/Grafana

---
+++ gateways/influx-gateway/src/main/java/com/bytex/snamp/gateway/influx/InfluxGatewayActivator.java
@@ -0,0 +1,20 @@
+package com.bytex.snamp.gateway.influx;
+
+import com.bytex.snamp.gateway.Gateway;
+import com.bytex.snamp.gateway.GatewayActivator;
+
+/**
+ * @author Roman Sakno
+ * @version 1.0
+ * @since 1.0
+ */
+public final class InfluxGatewayActivator extends GatewayActivator {
+    public InfluxGatewayActivator() {
+        super(InfluxGatewayActivator::newGateway);
+    }
+
+    private static Gateway newGateway(final String gatewayInstance,
+                                      final RequiredService<?>... dependencies) throws Exception{
+
+    }
+}
