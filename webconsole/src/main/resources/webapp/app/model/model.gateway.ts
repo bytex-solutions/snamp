d9eb1be29b7dc4504a7f16f10a92e5fc1e0ae34e
Evgeniy Kirichenko <evgeniy.kirichenko@gmail.com>
2016-12-03 01:12:55 +0300

    Removed all inline styles from gateway template. All necessary bindings are supported now

--- webconsole/src/main/resources/webapp/app/model/model.gateway.ts
+++ webconsole/src/main/resources/webapp/app/model/model.gateway.ts
@@ -7,16 +7,41 @@ import { Binding } from './model.binding';
 export class Gateway extends TypedEntity {
     http:ApiClient;
     attributes:Binding[] = [];
+    events:Binding[] = [];
+    operations:Binding[] = [];
     constructor(http:ApiClient, name:string, type:string, parameters: { [key:string]:string; }) {
         super(name, type, parameters);
         this.http = http;
         if (name != "") {
+            // retrieving attributes bindings
             this.http.get("/snamp/console/gateway/" + name + "/attributes/bindings")
                 .map((res: Response) => res.json())
                 .subscribe(response => {
                     for (let resourceKey in response) {
                         for (let attributeKey in response[resourceKey]) {
                             this.attributes.push(new Binding(attributeKey, resourceKey, response[resourceKey][attributeKey]));
+                        }
+                    }
+                });
+
+            // retrieving events bindings
+            this.http.get("/snamp/console/gateway/" + name + "/events/bindings")
+                .map((res: Response) => res.json())
+                .subscribe(response => {
+                    for (let resourceKey in response) {
+                        for (let eventKey in response[resourceKey]) {
+                            this.events.push(new Binding(eventKey, resourceKey, response[resourceKey][eventKey]));
+                        }
+                    }
+                });
+
+            // retrieving operations bindings
+            this.http.get("/snamp/console/gateway/" + name + "/operations/bindings")
+                .map((res: Response) => res.json())
+                .subscribe(response => {
+                    for (let resourceKey in response) {
+                        for (let operationKey in response[resourceKey]) {
+                            this.operations.push(new Binding(operationKey, resourceKey, response[resourceKey][operationKey]));
                         }
                     }
                 });
