9a7765e28a1bd3293520f3643adf3dd88387cd59
Evgeniy Kirichenko <evgeniy.kirichenko@bytex.solutions>
2016-12-13 00:55:11 +0300

    Smart mode has been prepared for implementation

--- webconsole/src/main/resources/webapp/app/configuration/configuration.resources.ts
+++ webconsole/src/main/resources/webapp/app/configuration/configuration.resources.ts
@@ -11,10 +11,22 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/toPromise';
 
 import { Overlay } from 'angular2-modal';
-import { Modal } from 'angular2-modal/plugins/vex';
+import {
+  VEXBuiltInThemes,
+  Modal,
+  DialogPreset,
+  DialogFormModal,
+  DialogPresetBuilder,
+  VEXModalContext,
+  VexModalModule
+} from 'angular2-modal/plugins/vex';
 
 @Component({
-  templateUrl: 'app/configuration/templates/resources.html'
+  templateUrl: 'app/configuration/templates/resources.html',
+  styleUrls: [
+    'app/configuration/templates/css/checkbox.css',
+    'app/configuration/components/templates/css/vex.css',
+    'app/configuration/components/templates/css/vex-theme-wireframe.css'],
 })
 export class ResourcesComponent implements OnInit {
 
@@ -25,6 +37,7 @@ export class ResourcesComponent implements OnInit {
    http:ApiClient;
    availableResources :any[] = [];
    availableGroups:string[] = [];
+   oldSmartMode = false;
 
    constructor(apiClient: ApiClient, overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal) {
         this.http = apiClient;
@@ -42,6 +55,7 @@ export class ResourcesComponent implements OnInit {
                 this.activeResource = (this.resources.length > 0) ? this.resources[0] : this.activeResource;
                 this.oldTypeValue = this.activeResource.type;
                 this.oldGroupValue = this.activeResource.groupName;
+                this.oldSmartMode = this.activeResource.smartMode;
             });
 
         // Get all the available bundles that belong to Resources
@@ -62,50 +76,86 @@ export class ResourcesComponent implements OnInit {
     setActiveResource(resource:Resource) {
         this.activeResource = resource;
         this.oldTypeValue = resource.type;
+        this.oldGroupValue = resource.groupName;
+        this.oldSmartMode = resource.smartMode;
     }
 
     changeType(event:any) {
-         let dialog = this.modal.confirm()
-            .isBlocking(true)
-            .keyboard(27)
-            .message("If you change resource type - all resources will be removed. Are you sure?")
-            .open()
-            .then((resultPromise) => {
-                return (<Promise<boolean>>resultPromise.result)
-                  .then((response) => {
-                    this.oldTypeValue = event.target.value;
-                    this.http.put(REST.RESOURCE_TYPE(this.activeResource.name), event.target.value);
-                    return response;
-                  })
-                  .catch(() => {
-                    this.activeResource.type = this.oldTypeValue;
-                    return false;
-                  });
+      this.modal.confirm()
+        .isBlocking(true)
+        .className(<VEXBuiltInThemes>'wireframe')
+        .keyboard(27)
+        .message("If you change resource type - all resources will be removed. Are you sure?")
+        .open()
+        .then((resultPromise) => {
+            return (<Promise<boolean>>resultPromise.result)
+              .then((response) => {
+                this.oldTypeValue = event.target.value;
+                this.http.put(REST.RESOURCE_TYPE(this.activeResource.name), event.target.value);
+                return response;
+              })
+              .catch(() => {
+                this.activeResource.type = this.oldTypeValue;
+                return false;
               });
+      });
     }
 
     changeGroup(event:any) {
-         let dialog = this.modal.confirm()
+      this.modal.confirm()
+        .isBlocking(true)
+        .className(<VEXBuiltInThemes>'wireframe')
+        .keyboard(27)
+        .message("Setting group for resource leads to appending all entities from the group to resource. " +
+            "The page is going to be refreshed. Proceed?")
+        .open()
+        .then((resultPromise) => {
+            return (<Promise<boolean>>resultPromise.result)
+              .then((response) => {
+                this.oldGroupValue = event.target.value;
+                this.http.put(REST.RESOURCE_GROUP(this.activeResource.name), event.target.value)
+                    .subscribe(data => {
+                        location.reload();
+                    });
+                return response;
+              })
+              .catch(() => {
+                this.activeResource.groupName = this.oldGroupValue;
+                return false;
+              });
+        });
+    }
+
+    triggerSmartMode(event:any) {
+        if (this.activeResource.smartMode == false) {
+          this.modal.confirm()
+            .className(<VEXBuiltInThemes>'wireframe')
             .isBlocking(true)
             .keyboard(27)
-            .message("Setting group for resource leads to appending all entities from the group to resource. " +
-                "The page is going to be refreshed. Proceed?")
+            .message("If you enable smartMode - all attributes, events and operations will be removed. Proceed?")
             .open()
             .then((resultPromise) => {
                 return (<Promise<boolean>>resultPromise.result)
                   .then((response) => {
-                    this.oldGroupValue = event.target.value;
-                    this.http.put(REST.RESOURCE_GROUP(this.activeResource.name), event.target.value)
+                    this.http.put(REST.ENTITY_PARAMETERS("resource", this.activeResource.name, "smartMode"), true)
                         .subscribe(data => {
-                            location.reload();
+                           this.oldSmartMode = true;
+                           this.activeResource.smartMode = true;
+                           location.reload();
                         });
                     return response;
                   })
                   .catch(() => {
-                    this.activeResource.groupName = this.oldGroupValue;
+                    this.activeResource.smartMode = this.oldSmartMode;
                     return false;
                   });
-              });
+             });
+         } else {
+            this.http.put(REST.ENTITY_PARAMETERS("resource", this.activeResource.name, "smartMode"), false)
+                .subscribe(data => {
+                   this.oldSmartMode = false;
+                });
+         }
     }
 
     saveConnectionString() {
