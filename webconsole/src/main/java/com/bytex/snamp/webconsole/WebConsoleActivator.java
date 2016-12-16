02358229cf0de2b0a82a44db93fa6ef92969346a
Evgeniy Kirichenko <evgeniy.kirichenko@bytex.solutions>
2016-12-16 18:42:12 +0300

    Static file serving now acts via xml annotation on POM. Sad but what to do, what to do - I cannot make DefaultServlet work - there are some troubles with bundle resource access inside of corresponding jetty components

--- webconsole/src/main/java/com/bytex/snamp/webconsole/WebConsoleActivator.java
+++ webconsole/src/main/java/com/bytex/snamp/webconsole/WebConsoleActivator.java
@@ -2,8 +2,6 @@ package com.bytex.snamp.webconsole;
 
 import com.bytex.snamp.SpecialUse;
 import com.bytex.snamp.core.AbstractServiceLibrary;
-import com.google.common.collect.ImmutableMap;
-import org.eclipse.jetty.servlet.DefaultServlet;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.http.HttpService;
@@ -11,18 +9,14 @@ import org.osgi.service.http.NamespaceException;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
-import java.net.URL;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Map;
-
-import static com.bytex.snamp.internal.Utils.acceptWithContextClassLoader;
 
 /**
  * The type Web console activator.
  */
 public final class WebConsoleActivator extends AbstractServiceLibrary {
-    private static final String STATIC_SERVLET_CONTEXT = "/snamp/*";
     private static final ActivationProperty<HttpService> HTTP_SERVICE_ACTIVATION_PROPERTY = defineActivationProperty(HttpService.class);
 
     private static final class WebConsoleServletProvider extends ProvidedService<Servlet, WebConsoleServlet>{
@@ -63,29 +57,15 @@ public final class WebConsoleActivator extends AbstractServiceLibrary {
         bundleLevelDependencies.add(new SimpleDependency<>(ConfigurationAdmin.class));
     }
 
-    private String getResourceBase(){
-        final URL resourceRoot = getClass().getClassLoader().getResource("webapp");
-        assert resourceRoot != null;
-        return resourceRoot.toExternalForm();
-    }
-
     @Override
     protected void activate(final ActivationPropertyPublisher activationProperties) throws Exception {
         final HttpService httpService = getDependencies().getDependency(HttpService.class);
-        //registering hosting servlet for static content with files for Web Console
-        acceptWithContextClassLoader(getClass().getClassLoader(),
-                httpService,
-                (publisher) -> publisher.registerServlet(STATIC_SERVLET_CONTEXT, new DefaultServlet(),
-                        new Hashtable<>(ImmutableMap
-                                .of("resourceBase", getResourceBase(), "pathInfoOnly", "true")
-                        ), null));
         activationProperties.publish(HTTP_SERVICE_ACTIVATION_PROPERTY, httpService);
     }
 
     @Override
     protected void deactivate(final ActivationPropertyReader activationProperties) throws Exception {
-        final HttpService httpService = activationProperties.getProperty(HTTP_SERVICE_ACTIVATION_PROPERTY);
-        httpService.unregister(STATIC_SERVLET_CONTEXT);
+
     }
 
     @Override
