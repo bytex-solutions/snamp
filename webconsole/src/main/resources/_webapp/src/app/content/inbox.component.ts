0b1001291d0333c68b651dceb3bfc1ceb6955968
Evgeniy Kirichenko <evgeniy.kirichenko@bytex.solutions>
2016-12-20 02:40:12 +0300

    Appended Gentelella UI framework as additional _webapp (now used as main, all the classes migration is required

---
+++ webconsole/src/main/resources/_webapp/src/app/content/inbox.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'home'
+  selector: 'inbox',  // <home></home>
+  // We need to tell Angular's Dependency Injection which providers are in our app.
+  providers: [
+  ],
+  // Our list of styles in our component. We may add more to compose many styles together
+  styleUrls: [ '../app.style.css' ],
+  // Every Angular template is first compiled by the browser before Angular runs it's compiler
+  templateUrl: './inbox.component.html'
+})
+export class Inbox {
+  // Set our default values
+  localState = { value: '' };
+
+  // TypeScript public modifiers
+  constructor() {
+  }
+
+  ngOnInit() {
+    console.log('hello `Inbox` component');
+    // this.title.getData().subscribe(data => this.data = data);
+    $('.collapse-link').on('click', function() {
+        var $BOX_PANEL = $(this).closest('.x_panel'),
+            $ICON = $(this).find('i'),
+            $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+
+        // fix for some div with hardcoded fix class
+        if ($BOX_PANEL.attr('style')) {
+            $BOX_CONTENT.slideToggle(200, function(){
+                $BOX_PANEL.removeAttr('style');
+            });
+        } else {
+            $BOX_CONTENT.slideToggle(200);
+            $BOX_PANEL.css('height', 'auto');
+        }
+
+        $ICON.toggleClass('fa-chevron-up fa-chevron-down');
+    });
+
+    $('.close-link').click(function () {
+        var $BOX_PANEL = $(this).closest('.x_panel');
+
+        $BOX_PANEL.remove();
+    });
+  }
+
+  submitState(value: string) {
+    console.log('submitState', value);
+    this.localState.value = '';
+  }
+}
+
+
+
+
