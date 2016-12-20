92352d2f6e77e55c2a82c62346f33552994028f7
Evgeniy Kirichenko <evgeniy.kirichenko@bytex.solutions>
2016-12-20 03:22:07 +0300

    Panels work fine now

--- webconsole/src/main/resources/_webapp/src/app/app.component.ts
+++ webconsole/src/main/resources/_webapp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 /*
  * Angular 2 decorators and services
  */
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, AfterViewInit } from '@angular/core';
 import 'style!css!less!font-awesome-webpack/font-awesome-styles.loader!font-awesome-webpack/font-awesome.config.js';
 import { AppState } from './app.service';
 
@@ -17,7 +17,7 @@ import { AppState } from './app.service';
   ],
   templateUrl: './app.component.html'
 })
-export class App {
+export class App implements AfterViewInit {
 
   constructor(
     public appState: AppState) {
@@ -26,6 +26,33 @@ export class App {
 
   ngOnInit() {
     console.log('Initial App State', this.appState.state);
+  }
+
+   ngAfterViewInit() {
+      $(document).ready(function(){
+         $('.collapse-link').on('click', function() {
+             var $BOX_PANEL = $(this).closest('.x_panel'),
+                 $ICON = $(this).find('i'),
+                 $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+
+             // fix for some div with hardcoded fix class
+             if ($BOX_PANEL.attr('style')) {
+                 $BOX_CONTENT.slideToggle(200, function(){
+                     $BOX_PANEL.removeAttr('style');
+                 });
+             } else {
+                 $BOX_CONTENT.slideToggle(200);
+                 $BOX_PANEL.css('height', 'auto');
+             }
+
+             $ICON.toggleClass('fa-chevron-up fa-chevron-down');
+         });
+
+         $('.close-link').click(function () {
+             $(this).closest('.x_panel').remove();
+         });
+
+      });
   }
 
 }
