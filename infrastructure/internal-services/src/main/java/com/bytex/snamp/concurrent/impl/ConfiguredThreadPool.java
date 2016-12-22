23ebf59a5c9bd7b806b76fcdd89d2998ca1d7dea
sakno <roman.sakno@bytex.solutions>
2016-12-23 00:36:21 +0300

    Remove redundant code from thread pool management layer

--- infrastructure/internal-services/src/main/java/com/bytex/snamp/concurrent/impl/ConfiguredThreadPool.java
+++ infrastructure/internal-services/src/main/java/com/bytex/snamp/concurrent/impl/ConfiguredThreadPool.java
@@ -3,188 +3,66 @@ package com.bytex.snamp.concurrent.impl;
 import com.bytex.snamp.concurrent.GroupedThreadFactory;
 import com.bytex.snamp.configuration.ThreadPoolConfiguration;
 
-import javax.annotation.Nonnull;
-import java.time.Duration;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import javax.annotation.concurrent.Immutable;
+import java.util.Objects;
 import java.util.concurrent.*;
 
 /**
  * Represents thread pool with its initial configuration.
  */
-final class ConfiguredThreadPool implements ExecutorService, ThreadPoolConfiguration {
-    private final ThreadPoolConfiguration configuration;
-    private final ExecutorService threadPool;
+final class ConfiguredThreadPool extends ThreadPoolExecutor implements ExecutorService {
+    @Immutable
+    private static final class ThreadPoolParameters{
+        private final int corePoolSize;
+        private final int maxPoolSize;
+        private final long keepAliveTime;
+        private final TimeUnit keepAliveTimeUnit;
+        private final BlockingQueue<Runnable> taskQueue;
+        private final ThreadFactory threadFactory;
 
-    ConfiguredThreadPool(final ThreadPoolConfiguration config, final String threadGroup) {
-        final GroupedThreadFactory threadFactory = new GroupedThreadFactory(threadGroup, config.getThreadPriority());
-        final BlockingQueue<Runnable> taskQueue;
-        final int corePoolSize;
-        if (config.getQueueSize() == INFINITE_QUEUE_SIZE) {
+        private ThreadPoolParameters(final ThreadPoolConfiguration config, final String threadGroup){
+            threadFactory = new GroupedThreadFactory(threadGroup, config.getThreadPriority());
+            if (config.getQueueSize() == ThreadPoolConfiguration.INFINITE_QUEUE_SIZE) {
                 /*
                     Using an unbounded queue  will cause new tasks to wait in the queue when all corePoolSize
                     threads are busy. Thus, no more than corePoolSize threads will ever be created.
                  */
-            if (config.getMaxPoolSize() == Integer.MAX_VALUE) {
-                taskQueue = new SynchronousQueue<>();
-                corePoolSize = config.getMinPoolSize();
+                if (config.getMaxPoolSize() == Integer.MAX_VALUE) {
+                    taskQueue = new SynchronousQueue<>();
+                    corePoolSize = config.getMinPoolSize();
+                } else {
+                    taskQueue = new LinkedBlockingQueue<>();
+                    corePoolSize = config.getMaxPoolSize();
+                }
             } else {
-                taskQueue = new LinkedBlockingQueue<>();
-                corePoolSize = config.getMaxPoolSize();
+                taskQueue = config.getMaxPoolSize() == Integer.MAX_VALUE ?
+                        new SynchronousQueue<>() :
+                        new ArrayBlockingQueue<>(config.getQueueSize());
+                corePoolSize = config.getMinPoolSize();
             }
-        } else {
-            taskQueue = config.getMaxPoolSize() == Integer.MAX_VALUE ?
-                    new SynchronousQueue<>() :
-                    new ArrayBlockingQueue<>(config.getQueueSize());
-            corePoolSize = config.getMinPoolSize();
+            maxPoolSize = config.getMaxPoolSize();
+            keepAliveTimeUnit = TimeUnit.MILLISECONDS;
+            keepAliveTime = config.getKeepAliveTime().toMillis();
         }
-        threadPool = new ThreadPoolExecutor(corePoolSize,
-                config.getMaxPoolSize(),
-                config.getKeepAliveTime().toMillis(),
-                TimeUnit.MILLISECONDS,
-                taskQueue,
-                threadFactory);
-        configuration = config;
     }
 
-    @Override
-    public int getMinPoolSize() {
-        return configuration.getMinPoolSize();
+    private final ThreadPoolConfiguration configuration;
+
+    private ConfiguredThreadPool(final ThreadPoolConfiguration config, final ThreadPoolParameters parameters){
+        super(parameters.corePoolSize,
+                parameters.maxPoolSize,
+                parameters.keepAliveTime,
+                parameters.keepAliveTimeUnit,
+                parameters.taskQueue,
+                parameters.threadFactory);
+        this.configuration = Objects.requireNonNull(config);
     }
 
-    @Override
-    public void setMinPoolSize(final int value) {
-        configuration.setMinPoolSize(value);
+    ConfiguredThreadPool(final ThreadPoolConfiguration config, final String threadGroup) {
+        this(config, new ThreadPoolParameters(config, threadGroup));
     }
 
-    @Override
-    public int getMaxPoolSize() {
-        return configuration.getMaxPoolSize();
-    }
-
-    @Override
-    public void setMaxPoolSize(final int value) {
-        configuration.setMaxPoolSize(value);
-    }
-
-    @Override
-    public Duration getKeepAliveTime() {
-        return configuration.getKeepAliveTime();
-    }
-
-    @Override
-    public void setKeepAliveTime(Duration value) {
-
-    }
-
-    @Override
-    public int getQueueSize() {
-        return INFINITE_QUEUE_SIZE;
-    }
-
-    @Override
-    public void setQueueSize(int value) {
-
-    }
-
-    @Override
-    public int getThreadPriority() {
-        return DEFAULT_THREAD_PRIORITY;
-    }
-
-    @Override
-    public void setThreadPriority(int value) {
-
-    }
-
-    @Override
-    public Map<String, String> getParameters() {
-        return configuration.getParameters();
-    }
-
-    @Override
-    public void shutdown() {
-        threadPool.shutdown();
-    }
-
-    @Override
-    public List<Runnable> shutdownNow() {
-        return threadPool.shutdownNow();
-    }
-
-    @Override
-    public boolean isShutdown() {
-        return threadPool.isShutdown();
-    }
-
-    @Override
-    public boolean isTerminated() {
-        return threadPool.isTerminated();
-    }
-
-    @Override
-    public boolean awaitTermination(final long timeout, @Nonnull final TimeUnit unit) throws InterruptedException {
-        return threadPool.awaitTermination(timeout, unit);
-    }
-
-    @Override
-    public <T> Future<T> submit(@Nonnull final Callable<T> task) {
-        return threadPool.submit(task);
-    }
-
-    @Override
-    public <T> Future<T> submit(@Nonnull final Runnable task, final T result) {
-        return threadPool.submit(task, result);
-    }
-
-    @Override
-    public Future<?> submit(@Nonnull final Runnable task) {
-        return threadPool.submit(task);
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(@Nonnull final Collection<? extends Callable<T>> tasks) throws InterruptedException {
-        return threadPool.invokeAll(tasks);
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(@Nonnull final Collection<? extends Callable<T>> tasks, final long timeout, @Nonnull final TimeUnit unit) throws InterruptedException {
-        return threadPool.invokeAll(tasks, timeout, unit);
-    }
-
-    @Override
-    public <T> T invokeAny(@Nonnull final Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-        return threadPool.invokeAny(tasks);
-    }
-
-    @Override
-    public <T> T invokeAny(@Nonnull final Collection<? extends Callable<T>> tasks, final long timeout, @Nonnull final TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return threadPool.invokeAny(tasks, timeout, unit);
-    }
-
-    @Override
-    public void execute(@Nonnull final Runnable command) {
-        threadPool.execute(command);
-    }
-
-    @Override
-    public int hashCode() {
-        return configuration.hashCode();
-    }
-
-    private boolean equals(final ThreadPoolConfiguration other){
-        return configuration.equals(other);
-    }
-
-    private boolean equals(final ExecutorService other){
-        return threadPool.equals(other);
-    }
-
-    @Override
-    public boolean equals(final Object other) {
-        return other instanceof ThreadPoolConfiguration ?
-                equals((ThreadPoolConfiguration) other) :
-                other instanceof ExecutorService && equals((ExecutorService) other);
+    boolean hasConfiguration(final ThreadPoolConfiguration configuration) {
+        return Objects.equals(this.configuration, configuration);
     }
 }
--- infrastructure/internal-services/src/main/java/com/bytex/snamp/concurrent/impl/ThreadPoolRepositoryImpl.java
+++ infrastructure/internal-services/src/main/java/com/bytex/snamp/concurrent/impl/ThreadPoolRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.bytex.snamp.concurrent.impl;
 
 import com.bytex.snamp.ExceptionPlaceholder;
+import com.bytex.snamp.concurrent.AbstractConcurrentResourceAccessor;
 import com.bytex.snamp.concurrent.ConcurrentResourceAccessor;
 import com.bytex.snamp.concurrent.ThreadPoolRepository;
 import com.bytex.snamp.configuration.ThreadPoolConfiguration;
@@ -29,12 +30,11 @@ import static com.bytex.snamp.concurrent.AbstractConcurrentResourceAccessor.Acti
 public final class ThreadPoolRepositoryImpl extends AbstractFrameworkService implements ThreadPoolRepository, Closeable {
     public static final String PID = CMThreadPoolParser.PID;
 
-    private final ConcurrentResourceAccessor<Map<String, ConfiguredThreadPool>> threadPools =
+    private final AbstractConcurrentResourceAccessor<Map<String, ConfiguredThreadPool>> threadPools =
             new ConcurrentResourceAccessor<>(new HashMap<>());
 
     private final ExecutorService defaultThreadPool = new ConfiguredThreadPool(DefaultThreadPoolConfiguration.getInstance(), "SnampThread");
     private final Logger logger = Logger.getLogger("SnampThreadPoolRepository");
-
 
     @Override
     public ExecutorService getThreadPool(final String name, final boolean useDefaultIfNotExists) {
@@ -64,71 +64,74 @@ public final class ThreadPoolRepositoryImpl extends AbstractFrameworkService imp
         return threadPools.read(services -> ImmutableSet.copyOf(services.keySet()).iterator());
     }
 
+    private static Action<Map<String, ConfiguredThreadPool>, Void, ExceptionPlaceholder> createThreadPoolMerger(final Dictionary<String, ?> properties, final Logger logger){
+        return new Action<Map<String, ConfiguredThreadPool>, Void, ExceptionPlaceholder>() {
+
+            private void removeThreadPools(final Map<String, ? extends ExecutorService> threadPools) {
+                ImmutableSet.copyOf(threadPools.keySet()).stream().filter(poolName -> properties.get(poolName) == null).forEach(poolName -> {
+                    final ExecutorService threadPool = threadPools.remove(poolName);
+                    if(threadPool != null)
+                        threadPool.shutdown();
+                });
+            }
+
+            private void addThreadPools(final Map<String, ConfiguredThreadPool> threadPools) {
+                final Enumeration<String> keys = properties.keys();
+                while (keys.hasMoreElements()) {
+                    final String poolName = keys.nextElement();
+                    switch (poolName) {
+                        case Constants.SERVICE_PID:
+                        case Constants.OBJECTCLASS:
+                        case DEFAULT_POOL:
+                            continue;
+                        default:
+                            //deserialize configuration
+                            final ThreadPoolConfiguration offeredConfig;
+                            try {
+                                offeredConfig = CMThreadPoolParser.deserialize(poolName, properties, getClass().getClassLoader());
+                                assert offeredConfig != null;
+                            } catch (final IOException e) {
+                                logger.log(Level.SEVERE, "Unable to read thread pool config");
+                                continue;
+                            }
+                            //if exists then compare and merge
+                            if (threadPools.containsKey(poolName)) {
+                                final ConfiguredThreadPool actualConfig = threadPools.get(poolName);
+                                if (!actualConfig.hasConfiguration(offeredConfig))    //replace with a new thread pool
+                                    actualConfig.shutdown();
+                            }
+                            threadPools.put(poolName, new ConfiguredThreadPool(offeredConfig, poolName));
+                    }
+                }
+            }
+
+            @Override
+            public Void apply(final Map<String, ConfiguredThreadPool> threadPools) {
+                removeThreadPools(threadPools);
+                addThreadPools(threadPools);
+                return null;
+            }
+        };
+    }
+
+    private static Void destroyThreadPools(final Map<?, ? extends ExecutorService> threadPools){
+        threadPools.values().forEach(ExecutorService::shutdown);
+        threadPools.clear();
+        return null;
+    }
+
     @Override
     public void updated(final Dictionary<String, ?> properties) throws ConfigurationException {
         if (properties == null) //remove all
-            threadPools.write(services -> {
-                services.clear();
-                return null;
-            });
+            threadPools.write(ThreadPoolRepositoryImpl::destroyThreadPools);
         else    //merge with runtime collection of thread pools
-            threadPools.write(new Action<Map<String, ConfiguredThreadPool>, Void, ExceptionPlaceholder>() {
-
-                private void removeThreadPools(final Map<String, ? extends ExecutorService> services) {
-                    ImmutableSet.copyOf(services.keySet()).stream().filter(poolName -> properties.get(poolName) == null).forEach(poolName -> {
-                        final ExecutorService threadPool = services.remove(poolName);
-                        if(threadPool != null)
-                            threadPool.shutdown();
-                    });
-                }
-
-                private void addThreadPools(final Map<String, ConfiguredThreadPool> services) {
-                    final Enumeration<String> keys = properties.keys();
-                    while (keys.hasMoreElements()) {
-                        final String poolName = keys.nextElement();
-                        switch (poolName) {
-                            case Constants.SERVICE_PID:
-                            case Constants.OBJECTCLASS:
-                            case DEFAULT_POOL:
-                                continue;
-                            default:
-                                //deserialize configuration
-                                final ThreadPoolConfiguration offeredConfig;
-                                try {
-                                    offeredConfig = CMThreadPoolParser.deserialize(poolName, properties, getClass().getClassLoader());
-                                    assert offeredConfig != null;
-                                } catch (final IOException e) {
-                                    logger.log(Level.SEVERE, "Unable to read thread pool config");
-                                    continue;
-                                }
-                                //if exists then compare and merge
-                                if (services.containsKey(poolName)) {
-                                    final ConfiguredThreadPool actualConfig = services.get(poolName);
-                                    if (!actualConfig.equals(offeredConfig))    //replace with a new thread pool
-                                        actualConfig.shutdown();
-                                }
-                                services.put(poolName, new ConfiguredThreadPool(offeredConfig, poolName));
-                        }
-                    }
-                }
-
-                @Override
-                public Void apply(final Map<String, ConfiguredThreadPool> services) {
-                    removeThreadPools(services);
-                    addThreadPools(services);
-                    return null;
-                }
-            });
+            threadPools.write(createThreadPoolMerger(properties, logger));
     }
 
     @Override
     public void close() {
         defaultThreadPool.shutdown();
-        threadPools.write(services -> {
-            services.values().forEach(ConfiguredThreadPool::shutdown);
-            services.clear();
-            return null;
-        });
+        threadPools.write(ThreadPoolRepositoryImpl::destroyThreadPools);
     }
 
     @Override
