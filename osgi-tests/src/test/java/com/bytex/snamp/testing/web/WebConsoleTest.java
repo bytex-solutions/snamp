ff6bc6d61c486690e48694c7fd510a6a298af02e
Evgeniy Kirichenko <evgeniy.kirichenko@bytex.solutions>
2016-12-26 23:04:31 +0300

    Some tests have been moved from http management test class to web console one

--- osgi-tests/src/test/java/com/bytex/snamp/testing/web/WebConsoleTest.java
+++ osgi-tests/src/test/java/com/bytex/snamp/testing/web/WebConsoleTest.java
@@ -1,43 +1,76 @@
 package com.bytex.snamp.testing.web;
 
 import com.bytex.snamp.Acceptor;
-import com.bytex.snamp.configuration.AgentConfiguration;
-import com.bytex.snamp.testing.AbstractSnampIntegrationTest;
+import com.bytex.snamp.configuration.*;
+import com.bytex.snamp.gateway.GatewayActivator;
+import com.bytex.snamp.io.IOUtils;
+import com.bytex.snamp.testing.BundleExceptionCallable;
 import com.bytex.snamp.testing.SnampDependencies;
 import com.bytex.snamp.testing.SnampFeature;
+import com.bytex.snamp.testing.connector.jmx.AbstractJmxConnectorTest;
+import com.bytex.snamp.testing.connector.jmx.TestOpenMBean;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.junit.Test;
 import org.osgi.framework.BundleContext;
 
+import javax.management.AttributeChangeNotification;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import static com.bytex.snamp.testing.connector.jmx.TestOpenMBean.BEAN_NAME;
+
 /**
+ * The type Web console test.
+ *
  * @author Roman Sakno
  * @version 1.0
  * @since 1.0
  */
-@SnampDependencies({SnampFeature.STANDARD_TOOLS})
-public final class WebConsoleTest extends AbstractSnampIntegrationTest {
+@SnampDependencies({
+        SnampFeature.SNMP_GATEWAY,
+        SnampFeature.GROOVY_GATEWAY,
+        SnampFeature.NAGIOS_GATEWAY,
+        SnampFeature.NRDP_GATEWAY,
+        SnampFeature.SSH_GATEWAY,
+        SnampFeature.STANDARD_TOOLS,
+        SnampFeature.GROOVY_CONNECTOR,
+        SnampFeature.COMPOSITE_CONNECTOR
+})
+public final class WebConsoleTest extends AbstractJmxConnectorTest<TestOpenMBean> {
+
     private static final String WS_ENDPOINT = "ws://localhost:8181/snamp/console/events";
-    private final TestAuthenticator authenticator;
+    private static final String ADAPTER_INSTANCE_NAME = "test-snmp";
+    private static final String ADAPTER_NAME = "snmp";
+    private static final String SNMP_PORT = "3222";
+    private static final String SNMP_HOST = "127.0.0.1";
+    private static final String TEST_PARAMETER = "testParameter";
+
+
     private WebSocketClient client;
 
-    public WebConsoleTest(){
-        authenticator = new TestAuthenticator();
+    /**
+     * Instantiates a new Web console test.
+     *
+     * @throws MalformedObjectNameException the malformed object name exception
+     */
+    public WebConsoleTest() throws MalformedObjectNameException {
+        super(new TestOpenMBean(), new ObjectName(TestOpenMBean.BEAN_NAME));
     }
 
     @Override
-    protected void beforeStartTest(final BundleContext context) throws Exception {
-        client = new WebSocketClient();
-        client.start();
-    }
-
-    @Override
-    protected void afterCleanupTest(final BundleContext context) throws Exception {
-        client.stop();
-        client = null;
+    protected boolean enableRemoteDebugging() {
+        return false;
     }
 
     private <E extends Exception> void runTest(final Acceptor<? super Session, E> testBody) throws Exception {
@@ -48,18 +81,224 @@ public final class WebConsoleTest extends AbstractSnampIntegrationTest {
         }
     }
 
+    /**
+     * Log notification test.
+     */
     @Test
     public void logNotificationTest(){
 
     }
 
     /**
-     * Creates a new configuration for running this test.
+     * Test check simple resource with and without token.
      *
-     * @param config The configuration to set.
+     * @throws IOException              the io exception
+     * @throws InterruptedException     the interrupted exception
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     * @throws InvalidKeyException      the invalid key exception
+     * @throws SignatureException       the signature exception
      */
-    @Override
-    protected void setupTestConfiguration(final AgentConfiguration config) {
+    @Test
+    public void testGetStaticFiles() throws IOException, InterruptedException, NoSuchAlgorithmException,
+            InvalidKeyException, SignatureException {
 
+        Thread.sleep(2000);
+        // Test welcome files (no files are specified manually)
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8181/snamp/").openConnection();
+        connection.setRequestMethod("GET");
+        connection.setInstanceFollowRedirects(true);
+        connection.connect();
+        try {
+            assertEquals(HttpURLConnection.HTTP_OK, connection.getResponseCode());
+            final String attributeValue = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+            assertNotNull(attributeValue);
+            assertTrue(attributeValue.contains("<h3 class=\"masthead-brand\">SNAMP WEB UI</h3>"));
+
+        } finally {
+            connection.disconnect();
+        }
+        // Test some html file
+        connection = (HttpURLConnection) new URL("http://localhost:8181/snamp/login.html").openConnection();
+        connection.setRequestMethod("GET");
+        connection.setInstanceFollowRedirects(true);
+        connection.connect();
+        try {
+            assertEquals(HttpURLConnection.HTTP_OK, connection.getResponseCode());
+            final String attributeValue = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+            assertNotNull(attributeValue);
+            assertTrue(attributeValue.contains("<h1>Login to SNAMP UI</h1>"));
+
+        } finally {
+            connection.disconnect();
+        }
+        // Test assets
+        connection = (HttpURLConnection) new URL("http://localhost:8181/snamp/js/jquery.js").openConnection();
+        connection.setRequestMethod("GET");
+        connection.setInstanceFollowRedirects(true);
+        connection.connect();
+        try {
+            assertEquals(HttpURLConnection.HTTP_OK, connection.getResponseCode());
+            final String attributeValue = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+            assertNotNull(attributeValue);
+            assertTrue(attributeValue.contains("/*! jQuery v3.0.0 | (c) jQuery Foundation | jquery.org/license */"));
+
+        } finally {
+            connection.disconnect();
+        }
+
+        // Test file that does not exist
+        connection = (HttpURLConnection) new URL("http://localhost:8181/snamp/asdasdasdasd.ext").openConnection();
+        connection.setRequestMethod("GET");
+        connection.setInstanceFollowRedirects(true);
+        connection.connect();
+        try {
+            assertEquals(HttpURLConnection.HTTP_NOT_FOUND, connection.getResponseCode());
+        } finally {
+            connection.disconnect();
+        }
+
+    }
+
+    /**
+     * Dummy test.
+     *
+     * @throws InterruptedException the interrupted exception
+     */
+    @Test
+    public void dummyTest() throws InterruptedException {
+        Thread.sleep(10000000);
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    protected void beforeStartTest(final BundleContext context) throws Exception {
+        super.beforeStartTest(context);
+        client = new WebSocketClient();
+        client.start();
+        beforeCleanupTest(context);
+    }
+
+    @Override
+    protected void afterStartTest(final BundleContext context) throws Exception {
+        startResourceConnector(context);
+        syncWithGatewayStartedEvent(ADAPTER_NAME, (BundleExceptionCallable) () -> {
+            GatewayActivator.enableGateway(context, ADAPTER_NAME);
+            return null;
+        }, Duration.ofSeconds(30));
+
+        client.stop();
+        client = null;
+    }
+
+    @Override
+    protected void beforeCleanupTest(final BundleContext context) throws Exception {
+        GatewayActivator.disableGateway(context, ADAPTER_NAME);
+        stopResourceConnector(context);
+    }
+
+    @Override
+    protected void fillGateways(final EntityMap<? extends GatewayConfiguration> gateways) {
+        final GatewayConfiguration snmpAdapter = gateways.getOrAdd(ADAPTER_INSTANCE_NAME);
+        snmpAdapter.setType(ADAPTER_NAME);
+        snmpAdapter.getParameters().put("port", SNMP_PORT);
+        snmpAdapter.getParameters().put("host", SNMP_HOST);
+        snmpAdapter.getParameters().put(TEST_PARAMETER, "parameter");
+        snmpAdapter.getParameters().put("socketTimeout", "5000");
+        snmpAdapter.getParameters().put("context", "1.1");
+
+        // second instance of gateways for better console default content (dummyTest support)
+        final GatewayConfiguration snmpAdapterDummy = gateways.getOrAdd("new_snmp_adapter");
+        snmpAdapterDummy.setType(ADAPTER_NAME);
+        snmpAdapterDummy.getParameters().put("port", "3232");
+        snmpAdapterDummy.getParameters().put("host", SNMP_HOST);
+        snmpAdapterDummy.getParameters().put(TEST_PARAMETER, "parameter");
+        snmpAdapterDummy.getParameters().put("socketTimeout", "5000");
+        snmpAdapterDummy.getParameters().put("context", "1.2");
+    }
+
+    @Override
+    protected void fillAttributes(final EntityMap<? extends AttributeConfiguration> attributes) {
+        AttributeConfiguration attribute = attributes.getOrAdd("1.0");
+        attribute.setAlternativeName("string");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.1.0");
+
+        attribute = attributes.getOrAdd("2.0");
+        attribute.setAlternativeName("boolean");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.2.0");
+
+        attribute = attributes.getOrAdd("3.0");
+        attribute.setAlternativeName("int32");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.3.0");
+
+        attribute = attributes.getOrAdd("4.0");
+        attribute.setAlternativeName("bigint");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.4.0");
+
+        attribute = attributes.getOrAdd("5.1");
+        attribute.setAlternativeName("array");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.5.1");
+
+        attribute = attributes.getOrAdd("6.1");
+        attribute.setAlternativeName("dictionary");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.6.1");
+
+        attribute = attributes.getOrAdd("7.1");
+        attribute.setAlternativeName("table");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.7.1");
+
+        attribute = attributes.getOrAdd("8.0");
+        attribute.setAlternativeName("float");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("oid", "1.1.8.0");
+
+        attribute = attributes.getOrAdd("9.0");
+        attribute.setAlternativeName("date");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("displayFormat", "yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        attribute.getParameters().put("oid", "1.1.9.0");
+
+        attribute = attributes.getOrAdd("10.0");
+        attribute.setAlternativeName("date");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("displayFormat", "rfc1903-human-readable");
+        attribute.getParameters().put("oid", "1.1.10.0");
+
+        attribute = attributes.getOrAdd("11.0");
+        attribute.setAlternativeName("date");
+        attribute.getParameters().put("objectName", BEAN_NAME);
+        attribute.getParameters().put("displayFormat", "rfc1903");
+        attribute.getParameters().put("oid", "1.1.11.0");
+    }
+
+    @Override
+    protected void fillEvents(final EntityMap<? extends EventConfiguration> events) {
+        EventConfiguration event = events.getOrAdd(AttributeChangeNotification.ATTRIBUTE_CHANGE);
+        event.getParameters().put("severity", "notice");
+        event.getParameters().put("objectName", BEAN_NAME);
+
+        event = events.getOrAdd("com.bytex.snamp.connector.tests.impl.testnotif");
+        event.getParameters().put("severity", "panic");
+        event.getParameters().put("objectName", BEAN_NAME);
+
+        event = events.getOrAdd("com.bytex.snamp.connector.tests.impl.plainnotif");
+        event.getParameters().put("severity", "notice");
+        event.getParameters().put("objectName", BEAN_NAME);
     }
 }
