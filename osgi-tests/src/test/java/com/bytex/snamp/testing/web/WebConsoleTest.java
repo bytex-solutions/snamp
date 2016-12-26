2220e38d51c9e54ae0d73014a5f967e9119dc848
sakno <roman.sakno@bytex.solutions>
2016-12-26 22:44:48 +0300

    Test runner for websocket-based tests

--- osgi-tests/src/test/java/com/bytex/snamp/testing/web/WebConsoleTest.java
+++ osgi-tests/src/test/java/com/bytex/snamp/testing/web/WebConsoleTest.java
@@ -1,11 +1,17 @@
 package com.bytex.snamp.testing.web;
 
+import com.bytex.snamp.Acceptor;
 import com.bytex.snamp.configuration.AgentConfiguration;
 import com.bytex.snamp.testing.AbstractSnampIntegrationTest;
 import com.bytex.snamp.testing.SnampDependencies;
 import com.bytex.snamp.testing.SnampFeature;
+import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.junit.Test;
+import org.osgi.framework.BundleContext;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Roman Sakno
@@ -16,13 +22,30 @@ import org.junit.Test;
 public final class WebConsoleTest extends AbstractSnampIntegrationTest {
     private static final String WS_ENDPOINT = "ws://localhost:8181/snamp/console/events";
     private final TestAuthenticator authenticator;
+    private WebSocketClient client;
 
     public WebConsoleTest(){
         authenticator = new TestAuthenticator();
     }
 
-    private WebSocketClient createWebSocketClient(){
-        return null;
+    @Override
+    protected void beforeStartTest(final BundleContext context) throws Exception {
+        client = new WebSocketClient();
+        client.start();
+    }
+
+    @Override
+    protected void afterCleanupTest(final BundleContext context) throws Exception {
+        client.stop();
+        client = null;
+    }
+
+    private <E extends Exception> void runTest(final Acceptor<? super Session, E> testBody) throws Exception {
+        try(final Session session = client.connect(null, new URI(WS_ENDPOINT)).get(10, TimeUnit.SECONDS)){
+
+        } finally {
+            client.stop();
+        }
     }
 
     @Test
