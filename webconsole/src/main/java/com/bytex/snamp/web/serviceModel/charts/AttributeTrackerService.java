488f8e904913b8903c5a21b86f2861e0cbdb8f07
sakno <roman.sakno@bytex.solutions>
2017-01-05 18:54:24 +0300

    New logo for SNAMP

---
+++ webconsole/src/main/java/com/bytex/snamp/web/serviceModel/charts/AttributeTrackerService.java
@@ -0,0 +1,9 @@
+package com.bytex.snamp.web.serviceModel.charts;
+
+/**
+ * @author Roman Sakno
+ * @version 1.0
+ * @since 1.0
+ */
+public class AttributeTrackerService {
+}
---
+++ webconsole/src/main/java/com/bytex/snamp/web/serviceModel/charts/Dashboard.java
@@ -0,0 +1,10 @@
+package com.bytex.snamp.web.serviceModel.charts;
+
+/**
+ * Represents data model of dashboard with charts.
+ * @author Roman Sakno
+ * @version 2.0
+ * @since 2.0
+ */
+public final class ChartsDashboard {
+}
