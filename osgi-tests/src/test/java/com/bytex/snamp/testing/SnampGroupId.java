e1019f9ce0304792aef97b4392736f8b64abc2cb
sakno <roman.sakno@bytex.solutions>
2017-04-09 00:12:19 +0300

    Move all features into separated groupId

--- osgi-tests/src/test/java/com/bytex/snamp/testing/SnampGroupId.java
+++ osgi-tests/src/test/java/com/bytex/snamp/testing/SnampGroupId.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
  * @since 2.0
  */
 enum SnampGroupId {
-    FEATURES("com", "bytex", "snamp");
+    FEATURES("com", "bytex", "snamp", "features");
 
     private final String[] relativePath;
 
