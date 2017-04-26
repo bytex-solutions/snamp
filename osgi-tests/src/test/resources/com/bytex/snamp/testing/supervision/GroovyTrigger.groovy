5707d85499f019322a7f8c61ee5d9aa5e8732a13
Roman Sakno <roman.sakno@swisstok.ru>
2017-04-26 11:48:44 +0300

    Remove final keywors from trigger

--- osgi-tests/src/test/resources/com/bytex/snamp/testing/supervision/GroovyTrigger.groovy
+++ osgi-tests/src/test/resources/com/bytex/snamp/testing/supervision/GroovyTrigger.groovy
@@ -2,7 +2,7 @@ package com.bytex.snamp.testing.supervision
 
 import com.bytex.snamp.connector.health.HealthStatus
 
-public HealthStatus statusChanged(final HealthStatus previousStatus, final HealthStatus newStatus) {
+public HealthStatus statusChanged(HealthStatus previousStatus, HealthStatus newStatus) {
     System.out.println newStatus
     return newStatus
 }
