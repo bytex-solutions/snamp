45368b56926ae3a04aec0d55a1a4783394561bf2
sakno <roman.sakno@bytex.solutions>
2017-05-08 00:33:41 +0300

    Print information about scaling policies

--- infrastructure/management/src/main/java/com/bytex/snamp/management/shell/SupervisorInfoCommand.java
+++ infrastructure/management/src/main/java/com/bytex/snamp/management/shell/SupervisorInfoCommand.java
@@ -96,7 +96,7 @@ public final class SupervisorInfoCommand extends SupervisorConfigurationCommand 
             }
             checkInterrupted();
             if(showAutoScaling){
-                appendln(output, "==ELASTICITY MANAGEMENT==");
+                appendln(output, "==AUTO-SCALING==");
                 printAutoScalingConfig(supervisor.getAutoScalingConfig(), output);
             }
         } else
