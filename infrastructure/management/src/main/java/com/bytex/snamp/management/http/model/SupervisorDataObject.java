acc5d76474f4367bef278f34f74154285d20daac
sakno <roman.sakno@bytex.solutions>
2017-05-08 00:58:04 +0300

    Propagate auto-scaling config to REST API

--- infrastructure/management/src/main/java/com/bytex/snamp/management/http/model/SupervisorDataObject.java
+++ infrastructure/management/src/main/java/com/bytex/snamp/management/http/model/SupervisorDataObject.java
@@ -3,16 +3,22 @@ package com.bytex.snamp.management.http.model;
 import com.bytex.snamp.SpecialUse;
 import com.bytex.snamp.configuration.ScriptletConfiguration;
 import com.bytex.snamp.configuration.SupervisorConfiguration;
+import com.bytex.snamp.json.DurationDeserializer;
+import com.bytex.snamp.json.DurationSerializer;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonTypeName;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 import javax.annotation.Nonnull;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.google.common.base.Strings.nullToEmpty;
 
 /**
+ * Represents DTO for {@link SupervisorConfiguration}.
  * @author Roman Sakno
  * @version 2.0
  * @since 2.0
@@ -20,23 +26,40 @@ import static com.google.common.base.Strings.nullToEmpty;
 @JsonTypeName("supervisor")
 public final class SupervisorDataObject extends AbstractDataObject<SupervisorConfiguration> {
     private final Map<String, ScriptletDataObject> attributeCheckers;
+    private final Map<String, ScriptletDataObject> scalingPolicies;
     private ScriptletDataObject trigger;
     private String supervisorType;
     private String connectionStringTemplate;
+    private boolean autoScaling;
+    private int scalingSize;
+    private int maxClusterSize;
+    private int minClusterSize;
+    private Duration cooldownTime;
 
     @SpecialUse(SpecialUse.Case.SERIALIZATION)
     public SupervisorDataObject(){
         attributeCheckers = new HashMap<>();
+        scalingPolicies = new HashMap<>();
         supervisorType = SupervisorConfiguration.DEFAULT_TYPE;
         connectionStringTemplate = "";
+        cooldownTime = Duration.ZERO;
+        maxClusterSize = Integer.MAX_VALUE;
+        minClusterSize = 0;
+        scalingSize = 1;
     }
 
     public SupervisorDataObject(final SupervisorConfiguration configuration) {
         super(configuration);
         attributeCheckers = Exportable.importEntities(configuration.getHealthCheckConfig().getAttributeCheckers(), ScriptletDataObject::new);
+        scalingPolicies = Exportable.importEntities(configuration.getAutoScalingConfig().getPolicies(), ScriptletDataObject::new);
         trigger = new ScriptletDataObject(configuration.getHealthCheckConfig().getTrigger());
         supervisorType = configuration.getType();
         connectionStringTemplate = configuration.getDiscoveryConfig().getConnectionStringTemplate();
+        autoScaling = configuration.getAutoScalingConfig().isEnabled();
+        cooldownTime = configuration.getAutoScalingConfig().getCooldownTime();
+        scalingSize = configuration.getAutoScalingConfig().getScalingSize();
+        maxClusterSize = configuration.getAutoScalingConfig().getMaxClusterSize();
+        minClusterSize = configuration.getAutoScalingConfig().getMinClusterSize();
     }
 
     @Override
@@ -47,6 +70,12 @@ public final class SupervisorDataObject extends AbstractDataObject<SupervisorCon
         else
             trigger.exportTo(entity.getHealthCheckConfig().getTrigger());
         Exportable.exportEntities(attributeCheckers, entity.getHealthCheckConfig().getAttributeCheckers());
+        Exportable.exportEntities(scalingPolicies, entity.getAutoScalingConfig().getPolicies());
+        entity.getAutoScalingConfig().setEnabled(autoScaling);
+        entity.getAutoScalingConfig().setCooldownTime(cooldownTime);
+        entity.getAutoScalingConfig().setScalingSize(scalingSize);
+        entity.getAutoScalingConfig().setMaxClusterSize(maxClusterSize);
+        entity.getAutoScalingConfig().setMinClusterSize(minClusterSize);
         entity.setType(supervisorType);
         entity.getDiscoveryConfig().setConnectionStringTemplate(connectionStringTemplate);
     }
@@ -86,5 +115,62 @@ public final class SupervisorDataObject extends AbstractDataObject<SupervisorCon
 
     public void setConnectionStringTemplate(final String value){
         connectionStringTemplate = nullToEmpty(value);
+    }
+
+    @JsonProperty("scalingPolicies")
+    public Map<String, ScriptletDataObject> getScalingPolicies(){
+        return scalingPolicies;
+    }
+
+    public void setScalingPolicies(@Nonnull final Map<String, ScriptletDataObject> value){
+        scalingPolicies.clear();
+        scalingPolicies.putAll(value);
+    }
+
+    @JsonProperty("autoScaling")
+    public boolean isAutoScalingEnabled(){
+        return autoScaling;
+    }
+
+    public void setAutoScalingEnabled(final boolean value){
+        autoScaling = value;
+    }
+
+    @JsonProperty("cooldownTime")
+    @JsonSerialize(using = DurationSerializer.class)
+    public Duration getCooldownTime(){
+        return cooldownTime;
+    }
+
+    @JsonDeserialize(using = DurationDeserializer.class)
+    public void setCooldownTime(@Nonnull final Duration value){
+        cooldownTime = value;
+    }
+
+    @JsonProperty("scalingSize")
+    public int getScalingSize(){
+        return scalingSize;
+    }
+
+    public void setScalingSize(final int value){
+        scalingSize = value;
+    }
+
+    @JsonProperty("maxClusterSize")
+    public int getMaxClusterSize(){
+        return maxClusterSize;
+    }
+
+    public void setMaxClusterSize(final int value){
+        maxClusterSize = value;
+    }
+
+    @JsonProperty("minClusterSize")
+    public int getMinClusterSize(){
+        return minClusterSize;
+    }
+
+    public void setMinClusterSize(final int value){
+        minClusterSize = value;
     }
 }
