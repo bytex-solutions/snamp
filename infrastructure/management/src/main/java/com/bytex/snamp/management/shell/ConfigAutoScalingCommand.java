16d39455afd37a09d3287b5c216774e942c62aca
sakno <roman.sakno@bytex.solutions>
2017-05-08 20:09:47 +0300

    Configuration command for auto-scaling

--- infrastructure/management/src/main/java/com/bytex/snamp/management/shell/ConfigAutoScalingCommand.java
+++ infrastructure/management/src/main/java/com/bytex/snamp/management/shell/ConfigAutoScalingCommand.java
@@ -9,6 +9,7 @@ import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 
 import java.io.PrintWriter;
+import java.time.Duration;
 
 /**
  * Configures automatic scaling for group.
@@ -28,7 +29,42 @@ public final class ConfigAutoScalingCommand extends SupervisorConfigurationComma
     @Option(name = "-e", aliases = "--enable", description = "Enable or disable automatic scaling")
     private boolean enable;
 
+    @Option(name = "-d", aliases = "--delete", description = "Delete all scaling policies and disable automatic scaling")
+    private boolean del;
+
+    @Option(name = "-s", aliases = "--scaling-size", description = "Scaling size")
+    private int scalingSize = -1;
+
+    @Option(name = "-max", aliases = "--max-cluster-size", description = "Upper bound of scaling")
+    private int maxClusterSize = -1;
+
+    @Option(name = "-min", aliases = "--min-cluster-size", description = "Lower bound of scaling")
+    private int minClusterSize = -1;
+
+    @Option(name = "-c", aliases = "--cooldown", description = "Cooldown time, in millis")
+    private int cooldownTime = -1;
+
+
     private boolean processAutoScalingConfig(final SupervisorConfiguration.AutoScalingConfiguration configuration, final PrintWriter output) {
+        if(del){
+            configuration.getPolicies().clear();
+            configuration.setMaxClusterSize(Integer.MAX_VALUE);
+            configuration.setMinClusterSize(0);
+            configuration.setScalingSize(1);
+            configuration.setCooldownTime(Duration.ZERO);
+            configuration.setScalingSize(1);
+            configuration.setEnabled(false);
+        } else {
+            configuration.setEnabled(enable);
+            if(scalingSize > 0)
+                configuration.setScalingSize(scalingSize);
+            if(maxClusterSize > 0)
+                configuration.setMaxClusterSize(maxClusterSize);
+            if(minClusterSize >= 0)
+                configuration.setMinClusterSize(minClusterSize);
+            if(cooldownTime >= 0)
+                configuration.setCooldownTime(Duration.ofMillis(cooldownTime));
+        }
         return true;
     }
 
@@ -37,7 +73,7 @@ public final class ConfigAutoScalingCommand extends SupervisorConfigurationComma
         if (supervisors.containsKey(groupName))
             return processAutoScalingConfig(supervisors.get(groupName).getAutoScalingConfig(), output);
         else {
-            output.println("Supervisor doesn't exist");
+            output.print("Supervisor doesn't exist");
             return false;
         }
     }
