535faa07689cfc14fe45b56fbab2aab2ece2ce9f
sakno <roman.sakno@bytex.solutions>
2017-05-22 14:45:48 +0300

    Test for devops-tools

--- osgi-tests/src/test/resources/com/bytex/snamp/testing/supervision/GroovyTrigger.groovy
+++ osgi-tests/src/test/resources/com/bytex/snamp/testing/supervision/GroovyTrigger.groovy
@@ -1,8 +1,13 @@
 package com.bytex.snamp.testing.supervision
 
+@GrabConfig(initContextClassLoader = true)
+@Grab(group = 'org.codehaus.groovy', module = 'groovy-json', version = '2.4.5')
+import groovy.json.JsonSlurper
 import com.bytex.snamp.supervision.health.ResourceGroupHealthStatus
 
 public void statusChanged(ResourceGroupHealthStatus previousStatus, ResourceGroupHealthStatus newStatus) {
     System.out.println "Was ${previousStatus}"
     System.out.println "Became ${newStatus}"
+    final slurper = new JsonSlurper()
+    println slurper.parseText('56')
 }
