93dab98432f3ddc2612dbc56a97e3a6bab6ec29e
sakno <roman.sakno@bytex.solutions>
2017-11-27 21:56:49 +0200

    Refactoring of ActuatorConnector

--- connectors/actuator-connector/src/main/java/com/bytex/snamp/connector/actuator/ActuatorConnectorActivator.java
+++ connectors/actuator-connector/src/main/java/com/bytex/snamp/connector/actuator/ActuatorConnectorActivator.java
@@ -1,10 +1,11 @@
 package com.bytex.snamp.connector.actuator;
 
 import com.bytex.snamp.SpecialUse;
-import com.bytex.snamp.concurrent.ThreadPoolRepository;
+import com.bytex.snamp.configuration.ManagedResourceConfiguration;
 import com.bytex.snamp.connector.ManagedResourceActivator;
 
 import javax.annotation.Nonnull;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
@@ -13,19 +14,19 @@ import java.net.URISyntaxException;
  * @version 2.1
  * @since 2.0
  */
-public final class ActuatorConnectorActivator extends ManagedResourceActivator<ActuatorConnector> {
+public final class ActuatorConnectorActivator extends ManagedResourceActivator {
+    private static final class ActuatorConnectorFactory extends ManagedResourceLifecycleManager<ActuatorConnector>{
+
+        @Nonnull
+        @Override
+        protected ActuatorConnector createConnector(final String resourceName, final ManagedResourceConfiguration configuration) throws URISyntaxException {
+            return new ActuatorConnector(resourceName, new ActuatorConnectionOptions(new URI(configuration.getConnectionString()), configuration));
+        }
+    }
 
     @SpecialUse(SpecialUse.Case.OSGi)
     public ActuatorConnectorActivator() {
-        super(ActuatorConnectorActivator::createConnector,
-                configurationDescriptor(ActuatorConnectorDescriptionProvider::getInstance),
-                requiredBy(ActuatorConnector.class).require(ThreadPoolRepository.class));
-    }
-
-    @Nonnull
-    private static ActuatorConnector createConnector(final String resourceName,
-                                                     final ManagedResourceInfo configuration,
-                                                     final DependencyManager... dependencies) throws URISyntaxException {
-        return new ActuatorConnector(resourceName, configuration);
+        super(new ActuatorConnectorFactory(),
+                configurationDescriptor(ActuatorConnectorDescriptionProvider::getInstance));
     }
 }
